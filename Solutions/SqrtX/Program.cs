cbf6c8ef072116ad6c4d84db9c3522e7a3c0d060
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-11 17:25:28 +0200

    Solved "Sqrt(x)"

---
+++ Solutions/SqrtX/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SqrtX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 8;
+            Console.WriteLine(MySqrt(x)); //Expected: 2
+        }
+        
+        private static int MySqrt(int x)
+        {
+
+            double root = 1;
+            int i = 0;
+
+            while (true)
+            {
+                i = i + 1;
+                if(root == (x / root + root) / 2) break;
+                
+                root = (x / root + root) / 2;
+                if (i == x + 1) break;
+            }
+
+            return (int)root;
+        }
+    }
+}
