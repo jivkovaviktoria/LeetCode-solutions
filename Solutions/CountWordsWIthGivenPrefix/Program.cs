7f344e912b69039cb9d7728ed1a3e8cdae08aa61
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-11 11:41:13 +0200

    Solved "Count words with given prefix"

---
+++ Solutions/CountWordsWIthGivenPrefix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace CountWordsWIthGivenPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new string[] { "pay", "attention", "practice", "attend" };
+            var pref = "at";
+
+            Console.WriteLine(PrefixCount(words, pref));
+            //Expected: 2
+        }
+
+        private static int PrefixCount(string[] words, string pref) {
+            return words.Count(x => x.StartsWith(pref));
+        }
+    }
+}
