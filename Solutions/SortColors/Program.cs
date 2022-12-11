9b6a771b52a18500deed770db83bd5567913f52f
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-11 17:02:52 +0200

    Solved "Sort colors"

---
+++ Solutions/SortColors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SortColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 2, 0, 2, 1, 1, 0 };
+            
+            SortColors(nums);
+            Console.WriteLine(string.Join(",", nums)); //Expected: [0,0,1,1,2,2]
+        }
+
+        private static void SortColors(int[] nums) {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i + 1])
+                {
+                    (nums[i], nums[i + 1]) = (nums[i + 1], nums[i]);
+                    i = -1;
+                }
+            }
+        }
+    }
+}
