3ce06be2e2a0d7c10f6c4ce22fe67911dfaed426
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-11 11:44:25 +0200

    Solved "Find the highest altitude"

---
+++ Solutions/FindTheHighestAltitude/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindTheHighestAltitude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gain = new int[] { -5, 1, 5, 0, -7 };
+            
+            Console.WriteLine(LargestAltitude(gain));
+            //Expected: 1
+        }
+
+        private static int LargestAltitude(int[] gain) {
+            if(gain == null || gain.Length == 0) return 0;
+
+            int current = 0;
+            int max = 0;
+            for(int i = 0; i < gain.Length; i++)
+            {
+                current += gain[i];
+                if(current > max) max = current;
+            }
+
+            return max;
+        }
+    }
+}
