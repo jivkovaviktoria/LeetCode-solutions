8e1113bc9bbd639e96debc0d296ef63567ec6789
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-14 17:24:42 +0200

    Solved "Minimum cuts to divide a circle"

---
+++ Solutions/MinimumCutsToDivideACircle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MinimumCutsToDivideACircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 4;
+            Console.WriteLine(NumberOfCuts(n)); //Expected: 2
+        }
+
+        //We want to cut the circle in n equal parts so the only posible ways are:
+        
+        //If n is 1 we don't need to cut it
+        //If n is even - to cut it n/2 times
+        //If n is odd - to cut it n times
+        private static int NumberOfCuts(int n)
+        {
+            if(n == 1) return 0;
+            if(n%2 == 0) return n/2;
+            else return n;
+        }
+    }
+}
