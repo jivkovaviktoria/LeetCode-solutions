1813677fe973d4949387f9c3ffb54e94bda554ba
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-15 15:38:15 +0200

    Solved "Number of 1 bits"

---
+++ Solutions/NumberOf1Bits/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumberOf1Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint n = 00000000000000000000000000001011;
+            Console.WriteLine(HammingWeight(n)); //Expected: 3
+        }
+
+        //We use a bitwise & operator to isolate the rightmost bit in n,
+        //then check if it is equal to 1. If it is, we increment `res`.
+        //Then we shift n one bit to the righ (discarding the rightmost bit)
+        //This process is repeated until n becomes 0. Finally, we return the value of `res`
+        private static int HammingWeight(uint n) {
+
+            int res = 0;
+            while(n > 0)
+            {
+                uint x = n & 1;
+                if(x == 1) res++;
+                n = n >> 1;
+            }
+
+            return res;
+        }
+    }
+}
