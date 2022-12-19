2430a6ee86d4072acaae8ac269ab94648ffd7d20
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-19 13:35:29 +0200

    Solved "Merge sorted array"

---
+++ Solutions/MergeSortedArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MergeSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+            var nums2 = new int[] { 2, 5, 6 };
+            int m = nums1.Length, n = nums2.Length;
+            
+            Merge(nums1, m, nums2, n);
+            
+            //Expected: nums1 = [1,2,2,3,5,6]
+            Console.WriteLine(String.Join(",", nums1));
+        }
+
+        private static void Merge(int[] nums1, int m, int[] nums2, int n) {
+            for(var i = 0; i < nums2.Length; i++)
+                nums1[nums1.Length-1-i] = nums2[i]; //We append the elements of `nums2` to the end of `nums1`
+      
+            Array.Sort(nums1);
+        }
+    }
+}
