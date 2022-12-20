c5e291b1b29ff0e2dcf71882f80a4b51dc2cb746
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-20 12:07:22 +0200

    Solved "Is isomorphic"

---
+++ Solutions/IsIsomorphic/Description.md
@@ -0,0 +1,16 @@
+﻿
+<h1 align="center"> IsIsomorphic </h1>
+<br>
+
+Given two strings `s` and `t`, determine if they are isomorphic.
+
+Two strings `s` and `t` are isomorphic if the characters in `s` can be replaced to get `t`.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. 
+No two characters may map to the same character, but a character may map to itself.
+
+*Examples:*
+
+`Input: s = "egg", t = "add"` `Output: true`
+
+`Input: s = "foo", t = "bar"` `Output: false`
---
+++ Solutions/IsIsomorphic/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsIsomorphic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        //We first check if the lengths of s and t are equal.
+        //If they are not equal, we return false since it is not possible for two strings of different lengths to be isomorphic.
+        //Then we initialize a dictionary to store the character mapping and iterate over the characters in s and t.
+        //For each character, we check if the character in s has already been mapped to a character in t.
+        //If it has, we check if the character in t is the same as the one that was previously mapped.
+        //If the character in t is different, we return false. If the character in s has not been mapped
+        //to a character in t, we check if the character in t has already been mapped to a character in s.
+        //If it has, we return false. Otherwise, we add the mapping to the dictionary and continue iterating.
+        //If the loop completes - return true.
+        
+        public bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+            
+            var map = new Dictionary<char, char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c1 = s[i];
+                char c2 = t[i];
+                
+                if (map.ContainsKey(c1))
+                {
+                    if (map[c1] != c2) return false;
+                }
+                else
+                {
+                    if (map.ContainsValue(c2)) return false;
+                    map.Add(c1, c2);
+                }
+            }
+            
+            return true;
+        }
+    }
+}
