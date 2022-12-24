45006423a2d2e2b6ec2b2b313a3b4f8127a59199
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-24 11:00:17 +0200

    Solved "Ransom Note"

---
+++ Solutions/RansomNote/Description.md
@@ -0,0 +1,12 @@
+﻿<h1 align="center">Ransom Note</h1>
+<br>
+
+Given two strings `ransomNote` and `magazine`, return true if `ransomNote` can be constructed by using the letters from `magazine` and false otherwise.
+
+Each letter in `magazine` can only be used once in `ransomNote`.
+
+*Examples:*
+<br>
+
+`Input: ransomNote = "a", magazine = "b"` <br>
+`Output: false`
---
+++ Solutions/RansomNote/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RansomNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ransomNote = "aa";
+            var magazine = "aab";
+            Console.WriteLine(CanConstruct(ransomNote, magazine)); //Expected: true
+        }
+
+        //We store the frequency of each char that apperas in `magazine`, like that
+        //we keep track of the number of occurences of each character in `magazine`
+        //Then we iterate through each char in `ransomNote` and check if the current 
+        //char is in the `map`, if it's not we return false (because it's not possible
+        //to construct the ransomNote from the magazine), then we check if the count
+        //for that char is 0, that means there are no more occurrences of that char in the magazine
+        //so we return false. If it is not 0, we decrease the count and continue. 
+        //If we don't return false, we return true because we have succesfully chechked that all
+        //of the characters in the `ransomNote` can be fount in `magazine`
+        
+        private static bool CanConstruct(string ransomNote, string magazine)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+                for(int i = 0; i < magazine.Length; i++)
+                {
+                    if(map.ContainsKey(magazine[i]) == false)
+                        map.Add(magazine[i], 0);
+                    map[magazine[i]]++;
+                }
+                
+                for(int i = 0; i < ransomNote.Length; i++)
+                {
+                    if(map.ContainsKey(ransomNote[i]) == false) return false;
+                    else if(map[ransomNote[i]] == 0) return false;
+                    
+                    map[ransomNote[i]]--;
+                }
+                
+                return true;
+            }
+    }
+}
