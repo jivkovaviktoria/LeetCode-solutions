bae2924903b7bd24d9c2029c7cc1895f13366ca0
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-26 12:01:44 +0200

    Solved "Monotonic Array"

---
+++ Solutions/MonotonicArray/Description.md
@@ -0,0 +1,16 @@
+﻿An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all `i <= j`, `nums[i] <= nums[j]`. An array nums is monotone decreasing if for all `i <= j`, `nums[i] >= nums[j]`.
+
+Given an integer array `nums`, return `true` if the given array is monotonic, or `false` otherwise.
+<br>
+
+*Examples:*
+
+`Input: nums = [1,2,2,3]`<br>
+`Output: true`
+
+<br>
+
+`Input: nums = [1,3,2]`<br>
+`Output: false`
---
+++ Solutions/MonotonicArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MonotonicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 2 };
+            Console.WriteLine(IsMonotonic(nums)); //Expected: false
+        }
+
+        //The method starts by setting two boolean variables, incr and decr, to true. These variables are used to track
+        //whether the array is strictly increasing or strictly decreasing. The method then iterates through the array
+        //and compares each element to the next element. If the current element is greater than the next element, then incr
+        //is set to false. If the current element is less than the next element, then decr is set to false. This continues
+        //for each element in the array.
+
+        //After the loop finishes, the method checks if either incr or decr is still true. If either of them is true, then
+        //the array is monotonic and the method returns true. Otherwise, both incr and decr are false and the method returns
+        //false, indicating that the array is not monotonic.
+        private static bool IsMonotonic(int[] nums) {
+            bool incr = true, decr = true;
+            for(int i = 0; i < nums.Length-1; i++)
+            {
+                if(nums[i] > nums[i+1]) incr = false;
+                if(nums[i] < nums[i+1]) decr = false;
+            }
+        
+            if(incr || decr) return true;
+        
+            return false;
+        }
+    }
+}
