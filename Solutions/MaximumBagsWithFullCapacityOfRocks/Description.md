f86db2ab4745df6e90688c52f384322c81d6635a
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-27 14:01:37 +0200

    Solved "Maximum bags with full capacity of rocks"

---
+++ Solutions/MaximumBagsWithFullCapacityOfRocks/Description.md
@@ -0,0 +1,30 @@
+﻿<h1 align="center">Maximum Bags With Full Capacity of Rocks</h1>
+<br>
+
+
+You have `n` bags numbered from `0` to `n - 1`. 
+You are given two 0-indexed integer arrays `capacity`
+and `rocks`. The `i`*th* bag can hold a maximum of `capacity[i]`
+rocks and currently contains `rocks[i]` rocks. You are also
+given an integer `additionalRocks`, the number of additional
+rocks you can place in any of the bags.
+
+
+Return the maximum number of bags that could have 
+full capacity after placing the additional rocks in
+some bags.
+
+<br>
+
+*Examples:* <br>
+`Input: capacity = [2,3,4,5], rocks = [1,2,4,4], additionalRocks = 2`<br>
+`Output: 3`<br>
+
+*Explanation:<br>*
+
+Place 1 rock in bag 0 and 1 rock in bag 1.
+The number of rocks in each bag are now [2,3,4,4].
+Bags 0, 1, and 2 have full capacity.
+There are 3 bags at full capacity, so we return 3.
+It can be shown that it is not possible to have more than 3 bags at full capacity.
+Note that there may be other ways of placing the rocks that result in an answer of 3.
---
+++ Solutions/MaximumBagsWithFullCapacityOfRocks/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MaximumBagsWithFullCapacityOfRocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] capacity = new int[] { 2, 3, 4, 5 };
+            int[] rocks = new int[] { 1, 2, 4, 4};
+            int additionalRocks = 2;
+
+            Console.WriteLine(MaximumBags(capacity, rocks, additionalRocks)); //Expected: 3
+        }
+        
+
+        //This method appears to find the maximum number of bags that can be filled with a given number of additional rocks.
+        //It does this by calculating the difference between the capacity of each bag and the number of rocks it already
+        //contains (stored in the rocks array), and storing these values in the rocksNeeded array. It then sorts this array
+        //in ascending order and iteratively subtracts the values from additionalRocks, incrementing a count each time.
+        //If additionalRocks becomes negative at any point, the loop breaks and the count is returned as the result.
+        //This count represents the maximum number of bags that can be filled with the given number of additional rocks.
+        
+        private static int MaximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+            var rocksNeeded = new int[capacity.Length];
+            for(int i = 0; i < capacity.Length; i++)
+                rocksNeeded[i] = capacity[i] - rocks[i];
+        
+            Array.Sort(rocksNeeded);
+        
+            int count = 0;
+            foreach(var x in rocksNeeded){
+                additionalRocks -= x;
+                if(additionalRocks < 0) break;
+                count++;
+            }
+        
+            return count;
+        }
+    }
+}
