c3695204e0545d60117d06095cc635f91c2bb5b4
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-12-29 18:04:55 +0200

    Solved "Percentage of Letter in String"

---
+++ Solutions/PercentageOfLetterInString/Description.md
@@ -0,0 +1,20 @@
+﻿<h1 align="center">Percentage of Letter in String</h1>
+<br>
+
+Given a string s and a character letter, return the percentage of characters in s that equal letter rounded down to the nearest whole percent.
+
+
+Examples:
+
+`Input: s = "foobar", letter = "o"`<br>
+`Output: 33`<br>
+*Explanation:
+The percentage of characters in s that equal the letter 'o' is 2 / 6 * 100% = 33% when rounded down, so we return 33.*
+
+ 
+<br>
+
+`Input: s = "jjjj", letter = "k"`<br>
+`Output: 0`<br>
+*Explanation:
+The percentage of characters in s that equal the letter 'k' is 0%, so we return 0.*
---
+++ Solutions/PercentageOfLetterInString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PercentageOfLetterInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abca";
+            char letter = 'a';
+            Console.WriteLine(PercentageLetter(s, letter)); //Expected: 50
+        }
+        
+        //The method takes in a string called s and a character called letter as input.
+        //It initializes a count variable to zero, which will be used to keep track of the number of occurrences of letter
+        //in s. It then iterates through the characters in s using a for loop. For each character in the string, it checks
+        //if the character is equal to letter. If it is, it increments the count variable. After the loop has completed,
+        //it calculates the percentage of letter occurrences in s by multiplying the count by 100 and dividing it by the
+        //length of s. Finally, it returns the calculated percentage.
+
+        private static int PercentageLetter(string s, char letter) {
+            int count = 0;
+            for(int i = 0; i < s.Length; i++)
+                if(s[i] == letter) count++;
+        
+            return count*100/s.Length;
+        }
+    }
+}
