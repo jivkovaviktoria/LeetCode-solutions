1e3859ece4548a91e7c450dc00a9a0adb56aedf7
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-03 11:31:14 +0200

    Solved "Delete Columns to Make Sorted"

---
+++ Solutions/DeleteColumnsToMakeSorted/Description.md
@@ -0,0 +1,37 @@
+﻿<h1 align="center">Delete Columns to Make Sorted</h1>
+<br>
+
+You are given an array of `n` strings `strs`, all of the same length.
+
+The strings can be arranged such that there is one on each line, making a grid. For example, `strs = ["abc", "bce", "cae"]` can be arranged as:
+
+`abc`<br>
+`bce`<br>
+`cae`<br>
+
+You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
+
+Return the number of columns that you will delete.
+
+
+
+*Example 1:*
+
+`Input: strs = ["cba","daf","ghi"]`<br>
+`Output: 1`<br>
+
+Explanation: The grid looks as follows:
+cba
+daf
+ghi
+Columns 0 and 2 are sorted, but column 1 is not, so you only need to delete 1 column.
+
+*Example 2:*
+
+`Input: strs = ["a","b"]`<br>
+`Output: 0`
+
+Explanation: The grid looks as follows:
+a
+b
+Column 0 is the only column and is sorted, so you will not delete any columns.
---
+++ Solutions/DeleteColumnsToMakeSorted/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DeleteColumnsToMakeSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = new string[] { "cba", "daf", "ghi" };
+            Console.WriteLine(MinDeletionSize(strs)); //Expected: 1
+        }
+        
+        //The method then has a loop that goes through each column of the strings. For each column, it has another loop
+        //that goes through each string and compares it to the string before it. If the character in the current string
+        //is "less" than the character in the previous string (as determined by the comparison operator <) at the current
+        //column, the method increments "res" by 1 and breaks out of the inner loop. If the loop goes through all of the
+        //strings and no string has a character that is "less" than the character in the string before it, the method goes
+        //to the next column.
+
+        //Finally, the method returns the value of "res", which is the number of columns that need to be deleted to make the
+        //list of strings lexicographically sorted.
+        
+        public static int MinDeletionSize(string[] strs) {
+            int wordLength =strs[0].Length;
+            int res = 0;
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j][i] < strs[j - 1][i])
+                    {
+                        res++;
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
