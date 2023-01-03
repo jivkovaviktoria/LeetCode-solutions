67e41886fd499e53142beed2100cc22f94821b50
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-03 11:36:02 +0200

    Solved "Strong Password Checker II"

---
+++ Solutions/StrongPasswordCheckerII/Description.md
@@ -0,0 +1,30 @@
+﻿<h1 align="center">Strong Password Checker II</h1>
+<br>
+
+A password is said to be strong if it satisfies all the following criteria:
+
+It has at least 8 characters.<br>
+It contains at least one lowercase letter.<br>
+It contains at least one uppercase letter.<br>
+It contains at least one digit.<br>
+It contains at least one special character. <br>
+*The special characters are the characters in the following string: "!@#$%^&*()-+".*<br>
+
+It does not contain 2 of the same character in adjacent positions (i.e., "aab" violates this condition, but "aba" does not).<br>
+Given a string `password`, return `true` if it is a strong password. Otherwise, return `false`.
+
+
+
+*Example 1:*
+
+`Input: password = "IloveLe3tcode!"`<br>
+`Output: true`<br>
+
+Explanation: The password meets all the requirements. Therefore, we return true. <br>
+
+*Example 2:*
+
+`Input: password = "Me+You--IsMyDream"`<br>
+`Output: false`<br>
+
+Explanation: The password does not contain a digit and also contains 2 of the same character in adjacent positions. Therefore, we return false.
---
+++ Solutions/StrongPasswordCheckerII/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrongPasswordCheckerII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "IloveLe3tcode!";
+            Console.WriteLine(StrongPasswordCheckerII(password)); //Expected: true
+        }
+        
+        public static bool StrongPasswordCheckerII(string password) {
+            HashSet<char> hs = new HashSet<char>() {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+'};
+        
+            if(password.Length < 8) return false;
+        
+            bool lowerCase = false;
+            bool upperCase = false;
+            bool hasDigit = false;
+            bool hasSpCh = false;
+        
+            for(int i = 0; i < password.Length; i++)
+            {
+                if( i > 0 && password[i] == password[i-1]) return false;
+                if(Char.IsDigit(password[i])) hasDigit = true;
+                if(hs.Contains(password[i])) hasSpCh = true;
+                if(Char.IsLower(password[i])) lowerCase = true;
+                if(Char.IsUpper(password[i])) upperCase = true;
+            }
+        
+            if((lowerCase && upperCase) && hasDigit && hasSpCh) return true;
+            return false;
+        }
+    }
+}
