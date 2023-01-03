7d8aafe3b4e2d3e9b34212322685f006e6590a84
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-03 11:24:29 +0200

    Solved "Fizz Buzz"

---
+++ Solutions/FizzBuzz/Description.md
@@ -0,0 +1,20 @@
+﻿<h1 align="center">Fizz Buzz</h1>
+<br>
+
+Given an integer `n`, return a string array `answer` (1-indexed) where:
+
+`answer[i]` == `"FizzBuzz"` if i is divisible by 3 and 5.<br>
+`answer[i]` == `"Fizz"` if i is divisible by 3.<br>
+`answer[i]` == `"Buzz"` if i is divisible by 5.<br>
+`answer[i]` == `i` (as a string) if none of the above conditions are true.
+
+
+*Example 1:*
+
+`Input: n = 3`<br>
+`Output: ["1","2","Fizz"]`<br>
+
+*Example 2:*
+
+`Input: n = 5`<br>
+`Output: ["1","2","Fizz","4","Buzz"]`
---
+++ Solutions/FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            Console.WriteLine(string.Join(",", FizzBuzz(n))); //Expected: ["1","2","Fizz"]
+        }
+        
+
+        //The method first creates an empty list called "ans" that will eventually hold the answers. Then, it has a loop
+        //that starts at 1 and goes up to n. For each number in this loop, the method checks if it is divisible by both
+        //3 and 5. If it is, it adds "FizzBuzz" to the list "ans". If it is not, the method then checks if it is divisible
+        //by 3. If it is, it adds "Fizz" to the list "ans". If it is not divisible by 3, it then checks if it is divisible
+        //by 5. If it is, it adds "Buzz" to the list "ans". If it is not divisible by 3 or 5, it adds the number itself
+        //to the list "ans" as a string. Finally, the method returns the list "ans".
+        public static IList<string> FizzBuzz(int n) {
+            List<string> ans = new List<string>();
+            for(int i = 1; i <= n; i++)
+            {
+                if(i%3 == 0 && i%5 == 0) ans.Add("FizzBuzz");
+                else if(i%3==0) ans.Add("Fizz");
+                else if(i%5 == 0) ans.Add("Buzz");
+                else ans.Add(i.ToString());
+            }
+        
+            return ans;
+        }
+    }
+}
