026b65dceca990e4e72858fbe24495217c517ac3
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-03 13:08:41 +0200

    Solved "Find Target Indices After Sorting Array"

---
+++ Solutions/FindTargetIndicesAfterSortingArray/Description.md
@@ -0,0 +1,24 @@
+﻿<h1 align="center">Find Target Indices After Sorting Array</h1>
+<br>
+
+You are given a 0-indexed integer array `nums` and a target element `target`.
+
+A `target index` is an index `i` such that `nums[i] == target`.
+
+Return a list of the target indices of `nums` after sorting `nums` in *non-decreasing* order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
+
+*Example 1:*
+
+`Input: nums = [1,2,5,2,3], target = 2`<br>
+`Output: [1,2]`<br>
+
+Explanation: After sorting, nums is [1,2,2,3,5].
+The indices where nums[i] == 2 are 1 and 2. <br>
+
+*Example 2:*
+
+`Input: nums = [1,2,5,2,3], target = 3`<br>
+`Output: [3]`<br>
+
+Explanation: After sorting, nums is [1,2,2,3,5].
+The index where nums[i] == 3 is 3.
---
+++ Solutions/FindTargetIndicesAfterSortingArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindTargetIndicesAfterSortingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        
+        //This method takes in an array of integers called nums and an integer target. It returns a list of integers.
+        //The method first creates an empty list called ans. It then loops through each element, item, in nums, which has
+        //been sorted in ascending order. If an element is equal to the target, its index is added to the ans list. Finally,
+        //the ans list is returned.
+
+        private static IList<int> TargetIndices(int[] nums, int target) {
+            List<int> ans = new List<int>();
+        
+            int index = 0;
+            foreach(var item in nums.OrderBy(x => x))
+            {
+                if(item == target) ans.Add(index);
+                index++;
+            }
+        
+            return ans;
+        }
+    }
+}
