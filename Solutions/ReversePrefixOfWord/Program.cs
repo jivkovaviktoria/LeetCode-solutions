e7b84309e0d29c238426941cd74d429a1f20afca
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-05 18:33:50 +0200

    Solved "Reverse Prefix of Word"

---
+++ Solutions/ReversePrefixOfWord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace ReversePrefixOfWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "abcdefd";
+            char ch = 'd';
+            Console.WriteLine(ReversePrefix(word, ch)); //Expected: dcbaefd
+        }
+        
+        public static string ReversePrefix(string word, char ch) {
+            int index = word.IndexOf(ch);
+        
+            StringBuilder sb = new StringBuilder();
+            for(int i = index; i >= 0; i--)
+                sb.Append(word[i]);
+        
+            for(int i = index+1; i < word.Length; i++)
+                sb.Append(word[i]);
+        
+            return sb.ToString();
+        }
+    }
+}
