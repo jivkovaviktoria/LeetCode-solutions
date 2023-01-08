6282490e27ef0013ba0c2acc1b142cc2e73d7f1e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-08 09:59:06 +0200

    Solved "Perfect number"

---
+++ Solutions/PerfectNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckPerfectNumber(28)); //Expected: true
+        }
+        
+        //A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number
+        //itself. A divisor of an integer x is an integer that can divide x evenly.
+        public static bool CheckPerfectNumber(int num) {
+            int sum = 0;
+            for(int i = 1; i < num; i++)
+                if(num % i == 0) sum += i;
+        
+            if(sum == num) return true;
+            return false;
+        }
+    }
+}
