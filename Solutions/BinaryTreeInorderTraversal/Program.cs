8e145c93c01e3a3881cf99078377e7ea70a3b7e4
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-09 09:35:52 +0200

    Solved "Binary tree inorder traversal"

---
+++ Solutions/BinaryTreeInorderTraversal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTreeInorderTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3)));
+            Console.WriteLine(InorderTraversal(root)); //Expected: [1,3,2]
+        }
+        
+        public static IList<int> InorderTraversal(TreeNode root) {
+            List<int> ans = new List<int>();
+            Traverse(root);
+            return ans;
+        
+            void Traverse(TreeNode root)
+            {
+                if (root != null)
+                {
+                    Traverse(root.left);
+                    ans.Add(root.val);
+                    Traverse(root.right);
+                }
+            }
+        }
+    }
+    
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val=0, TreeNode left=null, TreeNode right=null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        } 
+    }
+}
