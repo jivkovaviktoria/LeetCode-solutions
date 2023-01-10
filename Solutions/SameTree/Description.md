dbd4225b9f93447e942cd353e96a7f1abb2629a2
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-10 09:23:37 +0200

    Solved "Same Tree"

---
+++ Solutions/SameTree/Description.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">Same Tree</h1>
+
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+ 
---
+++ Solutions/SameTree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SameTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+            TreeNode q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+            Console.WriteLine(IsSameTree(p, q)); //Expected true
+        }
+        
+        public static bool IsSameTree(TreeNode p, TreeNode q) {
+        
+            if (p == null && q != null) return false;
+            if (p != null && q == null) return false;
+            if (p == null && q == null) return true;
+            if (p.val != q.val) return false;
+        
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+    
+    public class TreeNode
+    {
+         public int val;
+         public TreeNode left;
+         public TreeNode right;
+         public TreeNode(int val=0, TreeNode left=null, TreeNode right=null)
+         {
+                 this.val = val;
+                 this.left = left;
+                 this.right = right;
+         } 
+    }
+}
