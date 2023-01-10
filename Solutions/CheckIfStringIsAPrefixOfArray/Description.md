56fc1da31c41d2d4cdddc1901a31984184ebd59d
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-10 09:20:37 +0200

    Solved "Check If String Is a Prefix of Array"

---
+++ Solutions/CheckIfStringIsAPrefixOfArray/Description.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">Check If String Is a Prefix of Array</h1>
+
+Given a string s and an array of strings words, determine whether s is a prefix string of words.
+
+A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
+
+Return true if s is a prefix string of words, or false otherwise.
---
+++ Solutions/CheckIfStringIsAPrefixOfArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace CheckIfStringIsAPrefixOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "iloveleetcode";
+            string[] words = new string[] { "i", "love", "leetcode", "apples" };
+
+            Console.WriteLine(IsPrefixString(s, words)); //Expected: true
+        }
+        
+        
+        public static bool IsPrefixString(string s, string[] words) {
+            StringBuilder sb = new StringBuilder();
+        
+            foreach(var w in words)
+            {
+                sb.Append(w);
+                if(s == sb.ToString()) return true;
+            }
+        
+            return false;
+        }
+    }
+}
