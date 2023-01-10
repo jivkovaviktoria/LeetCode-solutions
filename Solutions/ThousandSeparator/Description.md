c6584733dfe34aa5942d089a2c60220a10c7fd39
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-10 09:30:31 +0200

    Solved "Thousand Separator"

---
+++ Solutions/ThousandSeparator/Description.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Thousand Separator</h1>
+
+Given an integer n, add a dot (".") as the thousands separator and return it in string format.
---
+++ Solutions/ThousandSeparator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace ThousandSeparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1234;
+            Console.WriteLine(ThousandSeparator(n)); //Expected: 1.234
+        }
+        
+        public static string ThousandSeparator(int n) {
+        
+            string s = n.ToString(); 
+            if(n <  1000 && n >= 0) return s; 
+        
+            StringBuilder sb = new StringBuilder();   
+            int count = 0; 
+        
+            for(int i = s.Length-1; i >= 0; i--)
+            {    
+                if(count == 3)
+                {
+                    sb.Insert(0, '.');
+                    count=0;
+                }
+            
+                sb.Insert(0,s[i]); 
+                count++; 
+            }
+        
+            return sb.ToString(); 
+        }
+    }
+}
