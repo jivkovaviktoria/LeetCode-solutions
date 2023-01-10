80dd860d5c8425ea5e0cecafd065461079c500f2
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-10 10:12:48 +0200

    Solved "Maximum Count of Positive Integer and Negative Integer"

---
+++ Solutions/MaximumCountOfPositiveIntegerAndNegativeInteger/Description.md
@@ -0,0 +1,6 @@
+﻿<h1 align="center">Maximum Count of Positive Integer and Negative Integer</h1>
+
+Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+
+In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+<br>**Note that 0 is neither positive nor negative.**
---
+++ Solutions/MaximumCountOfPositiveIntegerAndNegativeInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumCountOfPositiveIntegerAndNegativeInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new[] { -2, -1, -3, 0, 0, 1, 2 };
+            Console.WriteLine(MaximumCount(nums)); //Expected: 3
+        }
+        
+        public static int MaximumCount(int[] nums)
+        {
+            return Math.Max(nums.Count(x => x < 0), nums.Count(x => x > 0));
+        }
+    }
+}
