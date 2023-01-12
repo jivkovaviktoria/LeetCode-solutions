adca962bd6b96ec9eeb7950176904360ed94c0ff
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-12 13:14:49 +0200

    Solved "Number of Lines to Write String"

---
+++ Solutions/NumberOfLinesToWriteString/Description.md
@@ -0,0 +1,10 @@
+﻿<h1 align="center">Number of Lines to Write String</h1>
+
+You are given a string s of lowercase English letters and an array widths denoting how many pixels wide each lowercase English letter is. Specifically, widths[0] is the width of 'a', widths[1] is the width of 'b', and so on.
+
+You are trying to write s across several lines, where each line is no longer than 100 pixels. Starting at the beginning of s, write as many letters on the first line such that the total width does not exceed 100 pixels. Then, from where you stopped in s, continue writing as many letters as you can on the second line. Continue this process until you have written all of s.
+
+Return an array result of length 2 where:
+
+result[0] is the total number of lines.
+result[1] is the width of the last line in pixels.
---
+++ Solutions/NumberOfLinesToWriteString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace NumberOfLinesToWriteString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        int[] widths = new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+	        string s = "abcdefghijklmnopqrstuvwxyz";
+
+	        Console.WriteLine(NumberOfLines(widths, s)); //Expected: [3,60]
+        }
+        
+        public static int[] NumberOfLines(int[] widths, string s) {
+	        var letters = s.Select(x => widths[x - 'a']);
+                
+        	int lines = 1;
+            int currLine = 0;
+        	foreach(var letter in letters)
+        	{
+        		if(currLine + letter > 100)
+        		{
+        			lines++;
+        			currLine = letter;
+        		}
+        		else currLine += letter;
+        	}
+                
+        	
+                return new[] {lines, currLine}; 
+        }
+    }
+}
