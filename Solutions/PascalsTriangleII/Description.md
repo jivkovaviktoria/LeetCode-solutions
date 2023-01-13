b46da7a0e1ae2356efcb3088bb8724be22b2c921
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-13 10:16:15 +0200

    Solved "Pascal's Triangle II"

---
+++ Solutions/PascalsTriangleII/Description.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Pascal's Triangle II</h1>
+
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
---
+++ Solutions/PascalsTriangleII/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PascalsTriangleII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 3;
+            Console.WriteLine(string.Join(",", GetRow(row))); //Expected: [1,3,3,1]
+        }
+        
+        public static IList<int> GetRow(int rowIndex) {
+            List<List<int>> res = new List<List<int>>();
+
+            for (int i = 0; i < rowIndex + 1; i++)
+            {
+                var rowList = new List<int>();
+                res.Add(rowList);
+                
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0 || j == i) rowList.Add(1);
+                    else rowList.Add(res[i - 1][j - 1] + res[i - 1][j]);
+                }
+            }
+        
+            return res[rowIndex];
+        }
+    }
+}
