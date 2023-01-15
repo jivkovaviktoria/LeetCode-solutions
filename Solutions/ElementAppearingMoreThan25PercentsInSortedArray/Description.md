1f4c9b4ab6c3071c83192e0099e6a851f8aeb69e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-15 14:47:35 +0200

    Solved "Element Appearing More Than 25% In Sorted Array"

---
+++ Solutions/ElementAppearingMoreThan25PercentsInSortedArray/Description.md
@@ -0,0 +1,3 @@
+﻿<h1 slot="center">Element Appearing More Than 25% In Sorted Array</h1>
+<br>
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
---
+++ Solutions/ElementAppearingMoreThan25PercentsInSortedArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElementAppearingMoreThan25PercentsInSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 2, 6, 6, 6, 6, 7, 10 };
+            Console.WriteLine(FindSpecialInteger(arr)); //Expected: 6
+
+            int[] arr2 = new int[] { 1, 1 };
+            Console.WriteLine(FindSpecialInteger(arr2)); //Expected: 1
+        }
+        
+        public static int FindSpecialInteger(int[] arr) {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(map.ContainsKey(arr[i]) == false) map.Add(arr[i], 0);
+                map[arr[i]]++;
+            }
+        
+            foreach(var (key, val) in map)
+                if(val > (arr.Length/4)) return key;
+        
+            return -1;
+        }
+    }
+}
