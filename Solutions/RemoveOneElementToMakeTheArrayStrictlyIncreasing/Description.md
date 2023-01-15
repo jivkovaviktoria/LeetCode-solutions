01dae8e391ff21f03ed85c5d330167f1b355bdc7
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-15 14:44:48 +0200

    Solved "Remove One Element to Make the Array Strictly Increasing"

---
+++ Solutions/RemoveOneElementToMakeTheArrayStrictlyIncreasing/Description.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Remove One Element to Make the Array Strictly Increasing</h1>
+
+Given a 0-indexed integer array nums, return true if it can be made strictly increasing after removing exactly one element, or false otherwise. If the array is already strictly increasing, return true.
+
+The array nums is strictly increasing if nums[i - 1] < nums[i] for each index (1 <= i < nums.length).
---
+++ Solutions/RemoveOneElementToMakeTheArrayStrictlyIncreasing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RemoveOneElementToMakeTheArrayStrictlyIncreasing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 10, 5, 7 };
+            Console.WriteLine(CanBeIncreasing(nums)); //Expected: true
+
+            int[] nums2 = new int[] { 2, 3, 1, 2 };
+            Console.WriteLine(CanBeIncreasing(nums2)); //Expected: false
+        }
+        
+         public static bool CanBeIncreasing(int[] nums) {
+               
+                int count = 0;
+                for (int i = 1; i < nums.Length && count < 2; i++)
+                {
+                    if (nums[i-1] >= nums[i])
+                    {
+                        count++;
+                        if (i > 1 && nums[i - 2] >= nums[i])
+                            nums[i] = nums[i - 1];
+                    }
+                }
+                
+                
+                return count < 2;
+            }
+    }
+}
