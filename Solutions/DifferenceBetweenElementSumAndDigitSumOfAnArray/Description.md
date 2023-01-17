b1c8dcb3f7da494472ef5c9c07249a1fa6ea6303
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-17 12:53:49 +0200

    Solved "Difference Between Element Sum and Digit Sum of an Array"

---
+++ Solutions/DifferenceBetweenElementSumAndDigitSumOfAnArray/Description.md
@@ -0,0 +1,9 @@
+﻿<h1 align="center">Difference Between Element Sum and Digit Sum of an Array</h1>
+
+You are given a positive integer array `nums`.
+
+The element sum is the sum of all the elements in nums.
+The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+Return the absolute difference between the element sum and digit sum of nums.
+
+Note that the absolute difference between two integers `x` and `y` is defined as `|x - y|`.
---
+++ Solutions/DifferenceBetweenElementSumAndDigitSumOfAnArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DifferenceBetweenElementSumAndDigitSumOfAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 15, 6, 3 };
+            Console.WriteLine(DifferenceOfSum(nums)); //Expected: 9
+
+            int[] nums2 = new int[] { 1, 2, 3, 4 };
+            Console.WriteLine(DifferenceOfSum(nums2)); //Expected: 0
+        }
+        
+        //We iterate through the array and add each element to a 'sum' variable, then break down each element
+        //into its digits, adding each digit to a 'dSum' variable, and returning the absolute difference between 'sum' and
+        //'dSum'.
+        public static int DifferenceOfSum(int[] nums) {
+            int sum = 0;
+            int dSum = 0;
+        
+            for(int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            
+                int num = nums[i];
+                while(num > 0)
+                {
+                    dSum+=num%10;
+                    num/=10;
+                }
+            }
+        
+            return Math.Abs(sum-dSum);
+        }
+    }
+}
