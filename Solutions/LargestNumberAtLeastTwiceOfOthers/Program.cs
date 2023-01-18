8ad450a74a147dc2281e20021a7c4c02e95d2a5c
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-18 09:37:29 +0200

    Solved "Largest Number At Least Twice of Others"

---
+++ Solutions/LargestNumberAtLeastTwiceOfOthers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace LargestNumberAtLeastTwiceOfOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 6, 1, 0 };
+            Console.WriteLine(DominantIndex(nums)); //Expected: 1
+        }
+        
+        // First we find the largest element in nums
+        // Then we iterate through the array and check if the element is at least twice greater than the element
+        // If it is not, we return =1;
+        public static int DominantIndex(int[] nums) {
+            int max = nums.OrderByDescending(x => x).First();
+        
+            int index = 0;
+            for(int i = 0; i < nums.Length; i++){
+                if(nums[i] == max) index = i;
+                else if((nums[i]*2 <= max) == false) return -1;
+            }
+        
+            return index;
+        }
+    }
+}
---
+++ Solutions/LargestNumberAtLeastTwiceOfOthers/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Largest Number At Least Twice of Others</h1>
+
+You are given an integer array nums where the largest integer is unique.
+
+Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
