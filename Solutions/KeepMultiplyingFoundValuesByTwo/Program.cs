902738dfe343488c147b3eac551b9c1759a8e353
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-18 09:31:35 +0200

    Solved "Keep Multiplying Found Values by Two"

---
+++ Solutions/KeepMultiplyingFoundValuesByTwo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KeepMultiplyingFoundValuesByTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 5, 3, 6, 1, 12 };
+            int original = 3;
+
+            Console.WriteLine(FindFinalValue(nums, original)); //Expected: 24
+        }
+        
+        public static int FindFinalValue(int[] nums, int original) {
+            HashSet<int> hs = new HashSet<int>(nums);
+            while(hs.Contains(original)){
+                original = original*2;
+            }
+        
+            return original;
+        }
+    }
+}
---
+++ Solutions/KeepMultiplyingFoundValuesByTwo/ReadMe.md
@@ -0,0 +1,10 @@
+﻿<h1 align="center">Keep Multiplying Found Values by Two</h1>
+
+You are given an array of integers `nums`. You are also given an integer original which is the first number that needs to be searched for in nums.
+
+You then do the following steps:
+
+If `original` is found in `nums`, multiply it by two (i.e., set original = 2 * original).
+Otherwise, stop the process.
+Repeat this process with the new number as long as you keep finding the number.
+Return the final value of original.
