5836af83cb5a0ea7fb42ad7b38881003a1175714
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-18 09:23:15 +0200

    Solved "Sort Array by Increasing Frequency"

---
+++ Solutions/SortArrayByIncreasingFrequency/Description.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Sort Array by Increasing Frequency</h1>
+
+Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+Return the sorted array.
---
+++ Solutions/SortArrayByIncreasingFrequency/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortArrayByIncreasingFrequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 1, 2, 2, 2, 3 };
+            Console.WriteLine(string.Join(",", FrequencySort(nums))); //Expected: [3,1,1,2,2,2]
+
+            int[] nums2 = new int[] { 2, 3, 1, 3, 2 };
+            Console.WriteLine(string.Join(",", FrequencySort(nums2))); //Expected: [1,3,3,2,2]
+        }
+        
+        //We count the frequency of each number using dictionary
+        //Then sort the array by the frequency count of each number, and in case of a tie, we sort the numbers
+        //in descending order
+        public static int[] FrequencySort(int[] nums) {
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+        
+            for(int i = 0; i < nums.Length; i++){
+                if(freq.ContainsKey(nums[i]) == false) freq.Add(nums[i], 0);
+                freq[nums[i]]++;
+            }
+        
+            return nums.OrderBy(x => freq[x]).ThenByDescending(x => x).ToArray();
+        }
+    }
+}
