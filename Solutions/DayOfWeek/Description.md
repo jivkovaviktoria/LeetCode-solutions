8307844c4a08666716d947a42afd5bdc7df1d4ce
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-18 09:18:33 +0200

    Solved "Day of week"

---
+++ Solutions/DayOfWeek/Description.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">Day Of Week</h1>
+
+Given a date, return the corresponding day of the week for that date.
+
+The input is given as three integers representing the day, month and year respectively.
+
+Return the answer as one of the following values `{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}`.
---
+++ Solutions/DayOfWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 31, month = 8, year = 2019;
+            Console.WriteLine(DayOfTheWeek(day, month, year)); //Expected: Saturday
+
+            int day2 = 18, month2 = 7, year2 = 1999;
+            Console.WriteLine(DayOfTheWeek(day2, month2, year2)); //Expected: Sunday
+        }
+        
+        public static string DayOfTheWeek(int day, int month, int year) {
+            return new DateTime(year,month,day).DayOfWeek.ToString();
+        }
+    }
+}
