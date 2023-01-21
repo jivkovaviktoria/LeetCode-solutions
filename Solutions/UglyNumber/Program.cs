28f1e9acfaa94c6f1e7b9a11a8ad0cbf4b9fa1d1
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-21 11:28:30 +0200

    Solved "Ugly number"

---
+++ Solutions/UglyNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UglyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            Console.WriteLine(IsUgly(n)); //Expected: true
+
+            int n2 = 14;
+            Console.WriteLine(IsUgly(n2)); //Expected: false
+        }
+        
+        public static bool IsUgly(int n) {
+            if (n <= 0) return false;
+
+            while (n > 0)
+            {
+                if (n % 2 == 0){
+                    n = n / 2;
+                    continue;
+                }
+                if (n % 3 == 0){
+                    n = n / 3;
+                    continue;
+                }
+                if (n % 5 == 0){
+                    n = n / 5;
+                    continue;
+                }
+                break;
+            }
+
+            if (n == 1) return true;
+            return false;
+        }
+    }
+}
---
+++ Solutions/UglyNumber/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Ugly Number</h1>
+
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer `n`, return true if n is an ugly number.
