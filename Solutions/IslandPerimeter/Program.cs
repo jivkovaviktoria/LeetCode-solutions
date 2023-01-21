a6f7aea2a81b6e02f37c12eab58765a2aacf95f6
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-21 11:26:31 +0200

    Solved "Island Perimeter"

---
+++ Solutions/IslandPerimeter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IslandPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] grid = new int[][]
+            {
+                new int[] { 0, 1, 0, 0 },
+                new int[] { 1, 1, 1, 0 },
+                new int[] { 0, 1, 0, 0 },
+                new int[] { 1, 1, 0, 0 }
+            };
+
+            Console.WriteLine(IslandPerimeter(grid)); //Expected: 16
+        }
+        
+        public static int IslandPerimeter(int[][] grid) {
+            int ans = 0;
+        
+            int h = grid.Length;
+        
+            for (int i = 0; i < h; i++) {
+                int w = grid[i].Length;
+                for (int j = 0; j < w; j++) {
+                    if (grid[i][j] == 1) {
+                        if (i == 0 || grid[i-1][j] == 0) ans++;
+                        if (i == h-1 || grid[i+1][j] == 0) ans++;
+                        if (j == 0 || grid[i][j-1] == 0) ans++;
+                        if (j == w - 1 || grid[i][j+1] == 0) ans++;
+                    }
+                }
+            }
+        
+            return ans;
+        }
+    }
+}
---
+++ Solutions/IslandPerimeter/ReadMe.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">Island Perimeter</h1>
+
+You are given row x col grid representing a map where `grid[i][j] = 1` represents land and `grid[i][j] = 0` represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
