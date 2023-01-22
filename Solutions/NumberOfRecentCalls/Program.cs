7eeb2c48cc52ccf97c5b8aed5862e441e23e06eb
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-22 18:14:16 +0200

    Solved "Number of Recent Calls"

---
+++ Solutions/NumberOfRecentCalls/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberOfRecentCalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecentCounter counter = new RecentCounter();
+            List<int> result = new List<int>();
+            
+            result.Add(counter.Ping(1));
+            result.Add(counter.Ping(100));
+            result.Add(counter.Ping(3001));
+            result.Add(counter.Ping(3002));
+
+            Console.WriteLine(string.Join(",", result)); //Expected: null,1,2,3,3
+        }
+    }
+    
+    public class RecentCounter {
+
+        private Queue<int> pings = new Queue<int>();
+
+        public RecentCounter(){}
+
+        public int Ping(int t)
+        {
+            pings.Enqueue(t);
+            while (pings.Any())
+            {
+                if (t - pings.Peek() > 3000)
+                {
+                    pings.Dequeue();
+                    continue;
+                }
+                
+                break;
+            }
+
+            return pings.Count;
+        }
+    }
+}
