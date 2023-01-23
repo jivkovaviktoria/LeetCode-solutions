5bcdac494a32d41753047564557de5d041050396
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-23 10:20:57 +0200

    Solved "Power of Two"

---
+++ Solutions/PowerOfTwo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 16;
+            int n2 = 3;
+
+            Console.WriteLine(IsPowerOfTwo(n)); //Expected: true
+            Console.WriteLine(IsPowerOfTwo(n2)); //Expected: false
+        }
+        
+        /// <summary>
+        /// This method checks if the given number is power of 2
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>bool</returns>
+        public static bool IsPowerOfTwo(int number) {
+            if(number < 1) return false;
+            while(number > 1){
+                if(number%2 != 0) return false;
+                number/=2;
+            }
+        
+            return true;
+        }
+    }
+}
---
+++ Solutions/PowerOfTwo/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Power of Two</h1>
+
+Given an integer `n`, return `true` if it is a power of two. Otherwise, return `false`.
+
+An integer `n` is a power of two, if there exists an integer `x` such that `n == 2x`.
