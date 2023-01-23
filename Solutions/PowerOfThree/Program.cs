4dd2378d19dca35decd145bc0fc108485006de56
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-23 10:37:01 +0200

    Solved "Power of Three"

---
+++ Solutions/PowerOfThree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PowerOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 27;
+            int n2 = 4;
+
+            Console.WriteLine(IsPowerOfThree(n)); //Expected: true
+            Console.WriteLine(IsPowerOfThree(n2)); //Expected: false
+        }
+        
+        /// <summary>
+        /// This method checks if the number is a power of 3
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>bool</returns>
+        public static bool IsPowerOfThree(int number) {
+            if(number < 1) return false;
+            while(number > 1){
+                if(number%3!=0) return false;
+                number/=3;
+            }
+        
+            return true;
+        }
+    }
+}
---
+++ Solutions/PowerOfThree/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Power of Three</h1>
+
+Given an integer `n`, return `true` if it is a power of three. Otherwise, return `false`.
+
+An integer `n` is a power of three, if there exists an integer `x` such that `n == 3x`.
