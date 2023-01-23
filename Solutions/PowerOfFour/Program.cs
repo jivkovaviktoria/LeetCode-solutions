27bf9078a132bfe89104972a1960f6221e6c4f9e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-23 10:17:23 +0200

    Solved "Power of Four"

---
+++ Solutions/PowerOfFour/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PowerOfFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 16;
+            int n2 = 5;
+
+            Console.WriteLine(IsPowerOfFour(n)); //Expected: true
+            Console.WriteLine(IsPowerOfFour(n2)); //Expected: false
+        }
+            
+        /// <summary>
+        /// This method ckecks if a given number is a power of 4.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>bool</returns>
+        public static bool IsPowerOfFour(int number) {
+            if (number < 1) return false;
+            
+            while(number > 1) {
+                if(number%4 != 0) return false;
+                number /= 4;
+            }
+            return true;
+        }   
+    }
+}
---
+++ Solutions/PowerOfFour/ReadMe.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">Power of Four</h1>
+
+Given an integer `n`, return `true` if it is a power of four. Otherwise, return `false`.
+
+An integer n is a power of four, if there exists an integer `x` such that `n == 4x`.
+
+ 
