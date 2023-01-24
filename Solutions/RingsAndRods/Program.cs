0b913b4bdbfc8bfa93c1cfd1a84aeadfee52e6df
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-24 14:14:24 +0200

    Solved "Rings and Rods"

---
+++ Solutions/RingsAndRods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RingsAndRods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rings = "B0B6G0R6R0R6G9";
+            Console.WriteLine(CountPoints(rings)); //Expected: 1
+        }
+        
+        public static int CountPoints(string rings) {
+            Dictionary<int, HashSet<char>> dic = new Dictionary<int, HashSet<char>>();
+        
+            for(int i = 0; i < rings.Length; i+=2)
+            {
+                var ring = rings[i+1] - '0';
+                var color = rings[i];
+                if(dic.ContainsKey(ring) == false) dic.Add(ring, new HashSet<char>());
+                dic[ring].Add(color);
+            }
+        
+            int ans = 0;
+            foreach(var (rod, hs) in dic)
+            {
+                if(hs.Count == 3)
+                    ans++;
+            }
+        
+            return ans;
+        
+        }
+    }
+}
---
+++ Solutions/RingsAndRods/ReadMe.md
@@ -0,0 +1,11 @@
+﻿<h1 align="center">Rings and Rods</h1>
+
+There are n rings and each ring is either red, green, or blue. The rings are distributed across ten rods labeled from 0 to 9.
+
+You are given a string rings of length 2n that describes the n rings that are placed onto the rods. Every two characters in rings forms a color-position pair that is used to describe each ring where:
+
+The first character of the ith pair denotes the ith ring's color ('R', 'G', 'B').
+The second character of the ith pair denotes the rod that the ith ring is placed on ('0' to '9').
+For example, "R3G2B1" describes n == 3 rings: a red ring placed onto the rod labeled 3, a green ring placed onto the rod labeled 2, and a blue ring placed onto the rod labeled 1.
+
+Return the number of rods that have all three colors of rings on them.
