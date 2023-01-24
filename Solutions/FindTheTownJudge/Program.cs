091bf48af40fa9164ec7778918a03757c71d02f8
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-24 14:02:14 +0200

    Solved "Find the Town Judge"

---
+++ Solutions/FindTheTownJudge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindTheTownJudge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            int[][] trust = new int[][] { new int[] { 1, 2 } };
+
+            Console.WriteLine(FindJudge(n, trust)); //Expected: 2
+        }
+        
+        public static int FindJudge(int n, int[][] trust) {
+            var map = new Dictionary<int,HashSet<int>>();
+        
+            for(int i =1; i <=n; i++)
+                map[i] = new HashSet<int>();
+            
+            for(int i = 0; i < trust.Length; i++)
+            {
+                map[trust[i][0]].Add(trust[i][1]);
+            }
+        
+            int judge = -1;   
+     
+            foreach(var item in map)
+            {
+                if(map[item.Key].Count == 0)
+                    judge = item.Key;
+            }
+        
+            for(int i = 1; i <=n; i++)
+            {
+                if(!map[i].Contains(judge) && judge != i)
+                    return -1;
+            }
+        
+        
+            return judge; 
+        }
+    }
+}
---
+++ Solutions/FindTheTownJudge/ReadMe.md
@@ -0,0 +1,12 @@
+﻿<h1 align="center">Find the Town Judge</h1>
+
+In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi.
+
+Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
