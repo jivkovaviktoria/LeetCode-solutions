d9bbe1055f03db72f941593de7a594e4645649c0
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-24 13:56:25 +0200

    Solved "Excel Sheet Column Number"

---
+++ Solutions/ExcelSheetColumnNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExcelSheetColumnNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string columnTitle = "A";
+            string columnTitle2 = "ZY";
+
+            Console.WriteLine(TitleToNumber(columnTitle)); //Expected: 1
+            Console.WriteLine(TitleToNumber(columnTitle2)); //Expected: 701
+        }
+        
+        public static int TitleToNumber(string columnTitle) {
+            Dictionary<char,int> dict = new Dictionary<char,int>();
+        
+            for(char c = 'A'; c <= 'Z'; c++){
+                dict.Add(c, c - 'A' + 1);
+            }
+        
+            int res = dict[columnTitle[0]];
+            for(int i = 1; i <columnTitle.Length; i++) {
+                res = (res * 26) + dict[columnTitle[i]];
+            }
+            return res;
+        }
+    }
+}
---
+++ Solutions/ExcelSheetColumnNumber/ReadMe.md
@@ -0,0 +1,14 @@
+﻿<h1 align="center">Excel Sheet Column Number</h1>
+
+Given a string `columnTitle` that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1<br>
+B -> 2<br>
+C -> 3<br>
+...<br>
+Z -> 26<br>
+AA -> 27<br>
+AB -> 28<br>
+...
