5696ea2ad891bbcb3cc24e20bce631e89a9a6004
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-25 17:08:19 +0200

    Solved "Greatest Common Divisor of Array"

---
+++ Solutions/FindGreatestCommonDivisorOfArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace FindGreatestCommonDivisorOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 5, 6, 9, 10 };
+
+            Console.WriteLine(FindGCD(nums)); //Expected: 2
+
+            int[] nums2 = new int[] { 7, 5, 6, 8, 3 };
+
+            Console.WriteLine(FindGCD(nums2)); //Expected: 1
+        }
+        
+        public static int FindGCD(int[] nums) {
+            int max = nums[0], min = nums[0];
+            foreach(int num in nums)
+            {
+                min = Math.Min(min, num);
+                max = Math.Max(max, num);
+            }
+        
+            for(int i = min; i > 1; --i)
+                if(min % i == 0 && max % i == 0)
+                    return i;
+        
+            return 1;
+        }
+    }
+}
---
+++ Solutions/FindGreatestCommonDivisorOfArray/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Find Greatest Common Divisor of Array</h1>
+
+Given an integer array `nums`, return the greatest common divisor of the smallest number and largest number in nums.
+
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
