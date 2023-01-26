9c518f6bcd62ad51ee11d5c115413e78cd999e4d
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-26 12:57:17 +0200

    Solved "Base 7"

---
+++ Solutions/Base7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Base7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            Console.WriteLine(ConvertToBase7(num)); //Expected: 202
+
+            int num2 = -7;
+            Console.WriteLine(ConvertToBase7(num2)); //Expected: -10
+        }
+        
+        public static string ConvertToBase7(int num)
+        {
+            if (num == 0) return "0";
+        
+            int n = Math.Abs(num);
+        
+            StringBuilder sb = new StringBuilder();
+        
+            while (n != 0)
+            {
+                var digit = n % 7;
+                sb.Insert(0, digit);
+                n /= 7;
+            }
+        
+            if (num < 0) sb.Insert(0, "-");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Solutions/Base7/ReadMe.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Base 7</h1>
+
+Given an integer `num`, return a string of its base 7 representation.
