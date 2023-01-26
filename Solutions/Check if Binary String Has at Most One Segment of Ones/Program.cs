ec7ea7f15c83037018b88a89b012a45eb6bdfc28
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-26 13:12:06 +0200

    Solved "Check if Binary String Has at Most One Segment of Ones"

---
+++ Solutions/Check if Binary String Has at Most One Segment of Ones/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "1001";
+            Console.WriteLine(CheckOnesSegment(s)); //Expected: false
+        }
+        
+        public static bool CheckOnesSegment(string s) {
+            if(s == "10" || s.Contains("0") == false) return true;
+            int idx1 = s.IndexOf("10");
+            int idx2 = s.IndexOf("01");
+        
+            if(idx1 > -1 && idx1 > idx2) return true;
+            return false;
+        }
+    }
+}
---
+++ Solutions/Check if Binary String Has at Most One Segment of Ones/ReadMe.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Check if Binary String Has at Most One Segment of Ones</h1>
+
+Given a binary string `s` without leading zeros, return `true` if s contains at most one contiguous segment of ones. Otherwise, return `false`.
