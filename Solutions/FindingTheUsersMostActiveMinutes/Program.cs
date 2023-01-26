1275412c5bcdcb60f647d39dfad06132986cee4e
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-26 20:32:11 +0200

    Solved "Finding the Users Active Minutes"

---
+++ Solutions/FindingTheUsersMostActiveMinutes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindingTheUsersMostActiveMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] logs = new int[][]
+            {
+                new int[] { 0, 5 },
+                new int[] { 1, 2 },
+                new int[] { 0, 2 },
+                new int[] { 0, 5 },
+                new int[] { 1, 3 }
+            };
+
+            int k = 5;
+
+            Console.WriteLine(string.Join(",", FindingUsersActiveMinutes(logs, k)));
+            // Expected: [0,2,0,0,0]
+        }
+        
+        public static int[] FindingUsersActiveMinutes(int[][] logs, int k) {
+            int[] ans = new int[k];
+        
+            Dictionary<int, HashSet<int>> map = new Dictionary<int, HashSet<int>>();
+        
+            for(int i = 0; i < logs.Length; i++){
+                if(map.ContainsKey(logs[i][0]) == false) map.Add(logs[i][0], new HashSet<int>());
+                map[logs[i][0]].Add(logs[i][1]);
+            }
+        
+            foreach(var (key, value) in map){
+                ans[value.Count-1]++;
+            }
+        
+            return ans;
+        }
+    }
+}
---
+++ Solutions/FindingTheUsersMostActiveMinutes/ReadMe.md
@@ -0,0 +1,11 @@
+﻿<h1 align="center">Finding the Users Active Minutes</h1>
+
+You are given the `logs` for `users` actions on LeetCode, and an integer `k`. The `logs` are represented by a 2D integer array `logs` where each `logs[i] = [IDi, timei]` indicates that the user with IDi performed an action at the minute timei.
+
+Multiple users can perform actions simultaneously, and a single user can perform multiple actions in the same minute.
+
+The user active minutes (UAM) for a given user is defined as the number of unique minutes in which the user performed an action on LeetCode. A minute can only be counted once, even if multiple actions occur during it.
+
+You are to calculate a 1-indexed array answer of size k such that, for each j (1 <= j <= k), answer[j] is the number of users whose UAM equals j.
+
+Return the array answer as described above.
