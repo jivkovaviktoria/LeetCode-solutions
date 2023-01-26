28d95c0a55eef86bb9cd29a5cdbd59022313a1d3
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-26 13:00:42 +0200

    Solved "Max Consecutive Ones"

---
+++ Solutions/MaxConsecutiveOnes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MaxConsecutiveOnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1,1,0,1,1,1};
+            Console.WriteLine(FindMaxConsecutiveOnes(nums)); //Expected: 3
+        }
+        
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int max = 0;
+            int current = 0;
+        
+            for(int i = 0; i < nums.Length; i++){
+                if(nums[i] == 1){
+                    current++;
+                    if(max < current) max = current;
+                }
+                else{
+                    current = 0;
+                }
+            }
+        
+            return max;
+        }
+    }
+}
---
+++ Solutions/MaxConsecutiveOnes/ReadMe.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Max Consecutive Ones</h1>
+
+Given a binary array `nums`, return the maximum number of consecutive 1's in the array.
