c726e98b0ef70d5faa50a8059a0feb5e71300959
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-26 13:06:32 +0200

    Solved "Longer Contiguous Segments of Ones than Zeros"

---
+++ Solutions/LongestContiguousSegmentsOfOnesThanZeros/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LongestContiguousSegmentsOfOnesThanZeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "1101";
+            Console.WriteLine(CheckZeroOnes(s)); //Expected: true
+        }
+        
+        public static bool CheckZeroOnes(string s) {
+            int maxOnes = 0, maxZeros = 0, currentOnes = 0, currentZeros = 0;
+        
+            for(int i = 0; i < s.Length; i++){
+                if(s[i] == '1'){
+                    currentZeros = 0;
+                    currentOnes++;
+                    if(maxOnes < currentOnes) maxOnes = currentOnes;
+                }
+                else{
+                    currentOnes = 0;
+                    currentZeros++;
+                    if(maxZeros < currentZeros) maxZeros = currentZeros;
+                }
+            }
+        
+            return maxOnes > maxZeros;
+        }
+    }
+}
---
+++ Solutions/LongestContiguousSegmentsOfOnesThanZeros/ReadMe.md
@@ -0,0 +1,6 @@
+﻿<h1 align="center">Longer Contiguous Segments of Ones than Zeros</h1>
+
+Given a binary string `s`, return `true` if the longest contiguous segment of 1's is strictly longer than the longest contiguous segment of 0's in s, or return `false` otherwise.
+
+For example, in `s = "110100010"` the longest continuous segment of `1s` has length 2, and the longest continuous segment of `0s` has length 3.
+Note that if there are no 0's, then the longest continuous segment of 0's is considered to have a length 0. The same applies if there is no 1's.
