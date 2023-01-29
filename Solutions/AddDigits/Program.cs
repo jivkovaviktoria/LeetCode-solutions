b42af570d71aacf0e3674102b5b34fe87bef2580
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-29 19:15:24 +0200

    Solved "Add Digits"

---
+++ Solutions/AddDigits/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AddDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 38;
+            Console.WriteLine(AddDigits(num)); //Expected: 2
+        }
+        
+        public static int AddDigits(int num) {
+            int n = num;
+        
+            while(n > 9){
+                int number = n;
+                int x = 0;
+                
+                while(number > 0){
+                    x += number%10;
+                    number/=10;
+                }
+            
+                n = x;
+            }
+        
+            return n;
+        
+        }
+    }
+}
---
+++ Solutions/AddDigits/ReadMe.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Add Digits</h1>
+
+Given an integer `num`, repeatedly add all its digits until the result has only one digit, and return it.
