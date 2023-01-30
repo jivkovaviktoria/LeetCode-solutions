5ecfe030d90f1938ea9ef57009e48f9a6891a66d
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-30 10:30:21 +0200

    Solved "Uncommon words from two sentences"

---
+++ Solutions/UncommonWordsFromTwoSentences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UncommonWordsFromTwoSentences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "this apple is sweet", s2 = "this apple is sour";
+            Console.WriteLine(string.Join(",", UncommonFromSentences(s1 ,s2)));
+            //Expected: ["sweet","sour"]
+        }
+        
+        public static string[] UncommonFromSentences(string s1, string s2) {
+            Dictionary<string, int> map = new Dictionary<string,int>();
+            List<string> result = new List<string>();
+        
+            foreach(string word in s1.Split()){
+                if(!map.ContainsKey(word)) map.Add(word, 1);
+                else map[word]++;
+            }
+        
+            foreach(string word in s2.Split()){
+                if(!map.ContainsKey(word)) map.Add(word, 1);
+                else map[word]++;
+            }
+        
+            foreach(var pair in map)
+                if(pair.Value == 1) result.Add(pair.Key);
+        
+            return result.ToArray();
+        }
+    }
+}
---
+++ Solutions/UncommonWordsFromTwoSentences/ReadMe.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">Uncommon Words From Two Sentences</h1>
+
+A sentence is a string of single-space separated words where each word consists only of lowercase letters.
+
+A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+
+Given two sentences `s1` and `s2`, return a list of all the uncommon words. You may return the answer in any order.
