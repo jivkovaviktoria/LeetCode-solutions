360136f95ba64ab8735c34844684ce37e98eaa61
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-30 10:44:57 +0200

    Solved "Fibonacci Number"

---
+++ Solutions/FibonacciNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FibonacciNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            Console.WriteLine(Fib(n)); //Expected: 2
+        }
+        
+        /// <summary>
+        /// This method calculates fibonacci of n
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns>int</returns>
+        public static int Fib(int n) {
+            if(n <= 0) return 0;
+        
+            int a = 0, b = 1, c = 1;
+        
+            for(int i = 1; i < n; i++){
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        
+            return c;
+        }
+    }
+}
---
+++ Solutions/FibonacciNumber/ReadMe.md
@@ -0,0 +1,8 @@
+﻿<h1 align="center">Fibonacci Number</h1>
+
+The Fibonacci numbers, commonly denoted `F(n)` form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+`F(0) = 0, F(1) = 1` <br>
+`F(n) = F(n - 1) + F(n - 2), for n > 1.`
+
+Given `n`, calculate `F(n)`.
