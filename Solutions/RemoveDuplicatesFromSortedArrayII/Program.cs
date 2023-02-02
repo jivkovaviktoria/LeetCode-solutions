4453172cd9a8dce44957c3074e9935e21d8ba8ea
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-02 09:57:06 +0200

    Solved "Remove Duplicates from Sorted Array II"

---
+++ Solutions/RemoveDuplicatesFromSortedArrayII/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RemoveDuplicatesFromSortedArrayII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 1, 1, 2, 2, 3 };
+            Console.WriteLine(RemoveDuplicates(nums)); //Expected: 5 , nums = [1,1,2,2,3,_]
+        }
+        
+        public static int RemoveDuplicates(int[] nums) {
+            int i = 0; 
+            foreach (var n in nums){ 
+                if (i < 2 || n > nums[i-2]) { 
+                    nums[i] = n; 
+                    i++;
+                }
+            }
+            
+            return i;
+        }
+    }
+}
