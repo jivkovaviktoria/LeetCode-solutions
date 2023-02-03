bae8234d5e1d3deb110d1eb34ab9fac6a39dceb1
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-03 16:06:42 +0200

    Solved "Greatest Common Divisor of Strings"

---
+++ Solutions/GreatestCommonDivisorOfStrings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GreatestCommonDivisorOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "ABCABC", str2 = "ABC";
+            Console.WriteLine(GcdOfStrings(str1, str2)); //Expected: ABC
+        }
+        
+        public static string GcdOfStrings(string str1, string str2) {
+        
+            for(int i = Math.Min(str1.Length, str2.Length); i >= 1; i--){
+                if(IsValid(i)) return str1.Substring(0, i);
+            }
+        
+            return "";
+        
+            bool IsValid(int x){
+                if(str1.Length % x == 0 && str2.Length % x == 0){
+                    string s = str1.Substring(0, x);
+                    return string.IsNullOrEmpty(str1.Replace(s, "")) && string.IsNullOrEmpty(str2.Replace(s, ""));
+                }
+            
+                return false;
+            }
+        }
+    }
+}
---
+++ Solutions/GreatestCommonDivisorOfStrings/ReadMe.md
@@ -0,0 +1,6 @@
+﻿<h1 align="center">Greatest Common Divisor of Strings</h1>
+
+For two strings `s` and `t`, we say "t divides s" if and only if <br>
+`s = t + ... + t` (i.e., t is concatenated with itself one or more times).
+
+Given two strings `str1` and `str2`, return the largest string `x` such that `x` divides both `str1` and `str2`.
