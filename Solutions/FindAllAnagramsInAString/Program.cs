a0a7880b3477bf56ac0496e4757c83eefe450b6b
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-05 15:20:34 +0200

    Solved "Find All Anagrams in a String"

---
+++ Solutions/FindAllAnagramsInAString/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindAllAnagramsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "cbaebabacd", p = "abc";
+            Console.WriteLine(string.Join(",", FindAnagrams(s, p)));
+            //Expected: [0,6]
+        }
+        
+        public static IList<int> FindAnagrams(string s, string p) {
+            if (s.Length < p.Length) return new List<int>();
+        
+            List<int> res = new List<int>();
+            int[] freqS = new int[26];
+            int[] freqP = new int[26];
+            char[] charP = p.ToCharArray();
+        
+            for (int i = 0; i < p.Length; i ++) freqP[charP[i] - 'a'] ++;
+        
+            for (int i = 0; i < s.Length - p.Length + 1; i ++)
+            {
+                if (!p.Contains(s.Substring(i, 1))) continue;
+                if (IsAnagram(s.Substring(i, p.Length), p)) res.Add(i);
+            }
+
+            return res;
+        
+            bool IsAnagram(string sub, string p)
+            {
+                freqS = new int[26];
+                char[] charS = sub.ToCharArray();
+                for (int i = 0; i < sub.Length; i ++) freqS[charS[i] - 'a'] ++;
+            
+                return Enumerable.SequenceEqual(freqS, freqP);
+            }
+        }
+    }
+}
---
+++ Solutions/FindAllAnagramsInAString/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Find All Anagrams in a String</h1>
+
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
