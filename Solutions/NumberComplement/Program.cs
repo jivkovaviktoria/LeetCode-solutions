00362848715a5a6f80feb9f5e89bca17da580a11
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-05 15:17:46 +0200

    Solved "Number Complement"

---
+++ Solutions/NumberComplement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace NumberComplement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5, num2 = 1;
+            Console.WriteLine(FindComplement(num2)); //Expected: 2
+            Console.WriteLine(FindComplement(num2)); //Expected: 0
+        }
+        
+        public static int FindComplement(int num) 
+         => Convert.ToInt32(string.Join("", Convert.ToString(num, 2).Select(x => x == '0' ? '1' : '0')), 2);
+        
+    }
+}
---
+++ Solutions/NumberComplement/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Number Complement</h1>
+The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+Given an integer num, return its complement.
