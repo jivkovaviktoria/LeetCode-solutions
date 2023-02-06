a35c9d4f5778d3f9d5ef89a98fb50c7c380b5092
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-06 14:09:43 +0200

    Solved "Separate the Digits in an Array"

---
+++ Solutions/SeparateDigitsInAnArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SeparateDigitsInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 13, 25, 83, 77 };
+            Console.WriteLine(string.Join(",", SeparateDigits(nums))); //Expected: [1,3,2,5,8,3,7,7]
+        }
+        
+        public static int[] SeparateDigits(int[] nums) {
+            List<int> ans = new List<int>();
+        
+            foreach(var num in nums){
+                Stack<int> digits = new Stack<int>();
+                if(num < 10) ans.Add(num);
+                else{
+                    var x = num;
+                    while(x > 0){
+                        digits.Push(x%10);
+                        x/=10;
+                    }
+                
+                    while(digits.Any()) ans.Add(digits.Pop());
+                }
+            }
+        
+            return ans.ToArray();
+        }
+    }
+}
---
+++ Solutions/SeparateDigitsInAnArray/ReadMe.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">Separate Digits in an Array</h1>
+
+Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+
+To separate the digits of an integer is to get all the digits it has in the same order.
+
+For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
