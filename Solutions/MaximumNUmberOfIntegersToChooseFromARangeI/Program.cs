9c194ce87a65f2726ef633a67903ecffbaf62f40
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-06 14:12:29 +0200

    Solved "Maximum Number of Integers to Choose From a Range I"

---
+++ Solutions/MaximumNUmberOfIntegersToChooseFromARangeI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumNUmberOfIntegersToChooseFromARangeI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] banned = new int[] { 1, 6, 5 };
+            int n = 5, maxSum = 6;
+            Console.WriteLine(MaxCount(banned, n, maxSum)); //Expected: 2
+        }
+        
+        public static int MaxCount(int[] banned, int n, int maxSum) {
+            int ans = 0;
+            int currentSum = 0;
+            for(int i = 1; i <= n; i++){
+                if(banned.Contains(i) == false && currentSum+i <= maxSum){
+                    currentSum+=i;
+                    ans++;
+                }
+            }
+            
+            return ans;
+        }
+    }
+}
---
+++ Solutions/MaximumNUmberOfIntegersToChooseFromARangeI/ReadMe.md
@@ -0,0 +1,9 @@
+﻿<h1 align="center">Maximum Number of Integers to Choose From a Range I</h1>
+
+You are given an integer array banned and two integers n and maxSum. You are choosing some number of integers following the below rules:
+
+The chosen integers have to be in the range [1, n].
+Each integer can be chosen at most once.
+The chosen integers should not be in the array banned.
+The sum of the chosen integers should not exceed maxSum.
+Return the maximum number of integers you can choose following the mentioned rules.
