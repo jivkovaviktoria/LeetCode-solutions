79fedd80cf744664405fdf1a377cb185d6334918
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-06 14:15:30 +0200

    Solved "Shuffle the Array"

---
+++ Solutions/ShuffleTheArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ShuffleTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 5, 1, 3, 4, 7 };
+            int n = 3;
+            Console.WriteLine(string.Join(",", Shuffle(nums, n))); //Expected: [2,3,5,4,1,7] 
+        }
+        
+        public static int[] Shuffle(int[] nums, int n) {
+        
+            int[] res = new int[n * 2];
+
+            for (int i = 0; i < n; i++)
+            {
+                res[i * 2] = nums[i];
+                res[i * 2 + 1] = nums[i + n];
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Solutions/ShuffleTheArray/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Shuffle the Array</h1>
+
+Given the array nums consisting of `2n` elements in the form `x1,x2,...,xn,y1,y2,...,yn]`.
+
+Return the array in the form `[x1,y1,x2,y2,...,xn,yn]`.
