3abbf7bc502ec63ae767554452ed29cb4281aa16
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-08 16:12:43 +0200

    Solved "Jump Game II"

---
+++ Solutions/JumpGameII/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace JumpGameII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 3, 1, 1, 4 };
+            Console.WriteLine(Jump(nums)); //Expected: 2
+        }
+        
+        public static int Jump(int[] nums) {
+        
+            int ans = 0;
+            int furthest = -1, target = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                furthest = Math.Max(nums[i] + i, furthest);
+                if (i == target)
+                {
+                    ans++;
+                    target = furthest;
+                }
+            }
+        
+            return ans; 
+        }
+    }
+}
---
+++ Solutions/JumpGameII/ReadMe.md
@@ -0,0 +1,11 @@
+﻿<h1 align="center">Jump Game II</h1>
+
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+0 <= j <= nums[i] and
+i + j < n
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+ 
