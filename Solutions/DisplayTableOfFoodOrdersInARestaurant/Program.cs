58798c8e19ddb4750d58f605559f9d72541572df
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-10 17:43:09 +0200

    Solved "Display Table of Food Orders in a Restaurant"

---
+++ Solutions/DisplayTableOfFoodOrdersInARestaurant/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DisplayTableOfFoodOrdersInARestaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IList<string>> orders = new List<IList<string>>()
+            {
+                new List<string>() { "David", "3", "Ceviche" },
+                new List<string>() { "Corina", "10", "Beef Burrito" },
+                new List<string>() { "David", "3", "Fried Chicken" },
+                new List<string>() { "Carla", "5", "Water" },
+                new List<string>() { "Carla", "5", "Ceviche" },
+                new List<string>() { "Rous", "3", "Ceviche" }
+            };
+
+            foreach (var row in DisplayTable(orders))
+                Console.WriteLine(string.Join(",", row));
+            
+            //Expected:
+            // Table,Beef Burrito,Ceviche,Fried Chicken,Water
+            // 3    ,0           ,2      ,1            ,0
+            // 5    ,0           ,1      ,0            ,1
+            // 10   ,1           ,0      ,0            ,0
+        }
+        
+        //We use a dictionary with string key (table) and another Dictionary as a value which stores the foodItem(key) and the count
+        //of this item. We have HashSet items so we can add every unique item and order them alphabeticaly for the result.
+        //We loop though the orders and add each order.
+        //We insert the first row of the result List (Table and then each item /order them alphabetically/).
+        //Then we loop though each order and add the count for each foodItem
+        
+        public static IList<IList<string>> DisplayTable(IList<IList<string>> orders) {
+            Dictionary<string, Dictionary<string, int>> ordersByTable = new Dictionary<string, Dictionary<string, int>>();
+            HashSet<string> items = new HashSet<string>();
+        
+            foreach(var order in orders){
+                var table = order[1];
+                var item = order[2];
+            
+            
+                if(ordersByTable.ContainsKey(table) == false){
+                    ordersByTable.Add(table, new Dictionary<string, int>());
+                }
+            
+                if(ordersByTable[table].ContainsKey(item) == false){
+                    ordersByTable[table].Add(item, 0);
+                }
+            
+                ordersByTable[table][item]++;
+                items.Add(item);
+            }
+        
+        
+            List<IList<string>> ans = new List<IList<string>>();
+        
+            var row = new List<string>();
+            row.Add("Table");
+        
+            var sorted = items.OrderBy(x => x, StringComparer.Ordinal);
+        
+            foreach(var item in sorted){
+                row.Add(item);
+            }
+        
+            ans.Add(row);
+        
+            foreach(var order in ordersByTable.OrderBy(x => int.Parse(x.Key))){
+                var currentRow = new List<string>();
+            
+                currentRow.Add(order.Key);
+            
+                foreach(var item in sorted){
+                    if(order.Value.ContainsKey(item)){
+                        currentRow.Add(order.Value[item].ToString());
+                    }
+                    else{
+                        currentRow.Add("0");
+                    }
+                }
+            
+                ans.Add(currentRow);
+            }
+        
+            return ans;
+        }
+    }
+}
---
+++ Solutions/DisplayTableOfFoodOrdersInARestaurant/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Display Table of Food Orders in a Restaurant</h1>
+
+Given the array `orders`, which represents the orders that customers have done in a restaurant. More specifically `orders[i]=[customerNamei,tableNumberi,foodItemi]` where `customerName i` is the name of the customer, tableNumberi is the table customer sit at, and `foodItem i` is the item customer orders.
+
+Return the restaurant's **“display table”**. The **“display table”** is a table whose row entries denote how many of each food item each table ordered. The first column is the table number and the remaining columns correspond to each food item in alphabetical order. The first row should be a header whose first column is “Table”, followed by the names of the food items. Note that the customer names are not part of the table. Additionally, the rows should be sorted in numerically increasing order.
