845b5cde1eda20988b83489dc9f924985f1a715a
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-15 09:41:55 +0200

    Solved "Reverse String II"

---
+++ Solutions/ReverseStringII/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseStringII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcdefg";
+            int k = 2;
+            Console.WriteLine(ReverseStr(s, k)); //Expected: "bacdfeg"
+        }
+        
+        public static string ReverseStr(string s, int k) {
+            return string.Concat(s
+                .Take(k).Reverse()
+                .Concat(s.Skip(k).Take(k))
+                .Concat(s.Length > 2 * k
+                    ? ReverseStr(s.Substring(2 * k), k)
+                    : string.Empty
+                )
+            );
+        }
+    }
+}
