b80d404a0e11137dcb5803347f7a02d138cc3351
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-15 09:44:10 +0200

    Solved "Add to Array-Form of Integer"

---
+++ Solutions/AddToArray-FormOfInteger/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddToArray_FormOfInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 0, 0 };
+            int k = 34;
+
+            Console.WriteLine(AddToArrayForm(nums, k)); //Expected: [1,2,3,4]
+        }
+        
+        public static IList<int> AddToArrayForm(int[] num, int k) {
+            List<int> ans = new List<int>();
+
+            Stack<int> digits = new Stack<int>();
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                int x = num[i] + k;
+                digits.Push(x % 10);
+                k = x / 10;
+            }
+
+            while (k > 0)
+            {
+                digits.Push(k % 10);
+                k = k / 10;
+            }
+
+            while(digits.Any()){
+                ans.Add(digits.Pop());
+            }
+        
+            return ans;
+        }
+    }
+}
---
+++ Solutions/AddToArray-FormOfInteger/ReadMe.md
@@ -0,0 +1,6 @@
+﻿<h1 align="center">Add to Array-Form of Integer</h1>
+
+The array-form of an integer num is an array representing its digits in left to right order.
+
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
