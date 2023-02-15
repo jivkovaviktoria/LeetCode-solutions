8c20b6c2b649f554eebd97d20a264e10604d1bb6
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-15 09:39:57 +0200

    Solved "Number of laser beams in a bank"

---
+++ Solutions/NumberOfLaserBeamsInABank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberOfLaserBeamsInABank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bank = new string[] { "000","111","000"};
+            Console.WriteLine(NumberOfBeams(bank));
+        }
+        
+        public static int NumberOfBeams(string[] bank) {
+                SortedDictionary<int, int> map = new SortedDictionary<int, int>();
+                
+                for(int i = 0; i < bank.Length; i++){
+                    map.Add(i, 0);
+                    for(int j = 0; j < bank[i].Length; j++){
+                        if(bank[i][j] == '1') map[i]++;
+                    }
+                }
+        
+                var values = map.Values.Where(x => x != 0).ToList();
+                int ans = 0;
+                for (int i = 0; i < values.Count-1; i++)
+                    ans += values[i] * values[i + 1];
+                        
+                return ans;
+            }
+    }
+}
---
+++ Solutions/NumberOfLaserBeamsInABank/ReadMe.md
@@ -0,0 +1,11 @@
+﻿<h1 align="center">Number of Laser Beams in a Bank</h1>
+
+Anti-theft security devices are activated inside a bank. You are given a 0-indexed binary string array bank representing the floor plan of the bank, which is an m x n 2D matrix. bank[i] represents the ith row, consisting of '0's and '1's. '0' means the cell is empty, while'1' means the cell has a security device.
+
+There is one laser beam between any two security devices if both conditions are met:
+
+The two devices are located on two different rows: r1 and r2, where r1 < r2.
+For each row i where r1 < i < r2, there are no security devices in the ith row.
+Laser beams are independent, i.e., one beam does not interfere nor join with another.
+
+Return the total number of laser beams in the bank.
