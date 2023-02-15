203d729fa3b3f221dcc608484fed280671dc0c42
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-15 09:46:10 +0200

    Solved "Find All Numbers Disappeared in an Array"

---
+++ Solutions/FindAllNumbersDisappearedInAnArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindAllNumbersDisappearedInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+            Console.WriteLine(FindDisappearedNumbers(nums)); //Expected: [5,6]
+        }
+        
+        public static IList<int> FindDisappearedNumbers(int[] nums) {
+            var hs = new HashSet<int>(nums);
+            var ans = new List<int>();
+        
+            for(int i = 1; i <= nums.Length; i++){
+                if(hs.Contains(i) == false) ans.Add(i);
+            }
+        
+            return ans;
+        }
+    }
+}
---
+++ Solutions/FindAllNumbersDisappearedInAnArray/ReadMe.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Find All Numbers Disappeared in an Array</h1>
+
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
