3ab842c6c5484433b5b10ecd71827a461e99f145
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-19 10:42:57 +0200

    Solved "Majority Element II"

---
+++ Solutions/MajorityElementII/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MajorityElementII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 2, 3 };
+            Console.WriteLine(MajorityElement(nums)); //Expected: 3
+        }
+        
+        public static IList<int> MajorityElement(int[] nums) {
+            int n = nums.Length/3;
+            HashSet<int> ans = new HashSet<int>();
+        
+            Dictionary<int, int> d = new Dictionary<int, int>();
+        
+            for(int i = 0; i < nums.Length; i++){
+                if(d.ContainsKey(nums[i]) == false) d.Add(nums[i], 0);
+                d[nums[i]]++;
+            
+                if(d[nums[i]] > n) ans.Add(nums[i]);
+            }
+        
+            return ans.ToList();
+        }
+    }
+}
---
+++ Solutions/MajorityElementII/ReadMe.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Majority Element II</h1>
+
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
