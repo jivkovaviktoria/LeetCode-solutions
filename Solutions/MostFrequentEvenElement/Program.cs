efc816a5c1aca479b197c0ebbf7a7d0a5143f643
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-19 10:52:35 +0200

    Solved "Most Frequent Even Element"

---
+++ Solutions/MostFrequentEvenElement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace MostFrequentEvenElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0, 1, 2, 2, 4, 4, 1 };
+            Console.WriteLine(MostFrequentEven(nums)); //Expected: 2
+        }
+        
+        public static int MostFrequentEven(int[] nums) {
+            Dictionary<int, int> d = new Dictionary<int,int>();
+        
+            for(int i = 0; i < nums.Length; i++){
+                if(d.ContainsKey(nums[i]) == false) d.Add(nums[i], 0);
+                d[nums[i]]++;
+            }
+        
+            var result = d.Where(x => x.Key % 2 == 0).OrderByDescending(x => x.Value).ThenBy(x => x.Key).FirstOrDefault().Key;
+        
+            if(result == 0 && d.Keys.Contains(0) == false) return -1;
+            return result;
+        }
+    }
+}
---
+++ Solutions/MostFrequentEvenElement/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Most Frequent Even Element</h1>
+
+Given an integer array nums, return the most frequent even element.
+
+If there is a tie, return the smallest one. If there is no such element, return -1.
