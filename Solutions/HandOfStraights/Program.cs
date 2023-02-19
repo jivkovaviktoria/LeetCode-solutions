2ddb21f03f6f25a020e37f80a5f9ca3729e480e4
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-19 10:39:48 +0200

    Solved "Hand of Straights"

---
+++ Solutions/HandOfStraights/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HandOfStraights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hand = new int[] { 1, 2, 3, 6, 2, 3, 4, 7, 8 };
+            Console.WriteLine(IsNStraightHand(hand, 3)); //Expected: true
+        }
+        
+        public static bool IsNStraightHand(int[] hand, int groupSize) {
+            SortedDictionary<int, int> d = new SortedDictionary<int, int>();
+        
+            foreach(var card in hand)
+            {
+                if(d.ContainsKey(card) == false) d.Add(card, 0);
+                d[card]++;
+            }
+        
+            while(d.Count > 0)
+            {
+                var start = d.Keys.First();
+                var size = groupSize;
+            
+                while(size > 0)
+                {
+                    if (d.ContainsKey(start))
+                    {
+                        d[start]--;
+                        if (d[start] == 0) d.Remove(start);
+                    }
+                    else return false;
+                
+                    start++;
+                    size--;
+                }
+            }
+        
+            return true;
+        }
+    }
+}
---
+++ Solutions/HandOfStraights/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Hand of Straights</h1>
+
+Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
+
+Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
