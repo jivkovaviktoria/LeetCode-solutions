a02ba20c3106da589f798c0d26172ebce9bf2f35
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-19 10:55:48 +0200

    Solved "Top K Frequent Elements"

---
+++ Solutions/TopKFrequentElements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopKFrequentElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 1, 1, 2, 2, 3 };
+            Console.WriteLine(string.Join(",", TopKFrequent(nums, 2))); //Expected: [1,2]
+        }
+        
+        public static int[] TopKFrequent(int[] nums, int k) {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+        
+            for(int i = 0; i < nums.Length; i++){
+                if(d.ContainsKey(nums[i]) == false) d.Add(nums[i], 0);
+                d[nums[i]]++;
+            }
+        
+            int[] ans = new int[k];
+            int index = 0;
+            int count = 0;
+        
+            foreach(var (key, value) in d.OrderByDescending(x => x.Value)){
+                if(count == k) break;
+                ans[index] = key;
+                index++;
+                count++;
+            }
+        
+            return ans;
+        }
+    }
+}
---
+++ Solutions/TopKFrequentElements/ReadMe.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Top K Frequent Elements</h1>
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
