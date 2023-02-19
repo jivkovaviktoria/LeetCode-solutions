17007fea650a66313a2290e7cc8e10fb8bccfdbb
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-19 10:57:36 +0200

    Solved "Most Common Word"

---
+++ Solutions/MostCommonWord/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace MostCommonWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p = "Bob hit a ball, the hit BALL flew far after it was hit.";
+            string[] banned = new string[] { "hit" };
+            Console.WriteLine(MostCommonWord(p, banned)); //Expected: ball
+        }
+        
+        public static string MostCommonWord(string paragraph, string[] banned) {
+            var d = paragraph.ToLower()
+                .Split(
+                    new[] { ' ', ',', '.', '?', '!', '\'', ';' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .GroupBy(w => w)
+                .ToDictionary(x => x.Key, x => x.Count());
+        
+            foreach(var word in banned) d.Remove(word);
+            return d.OrderByDescending(x => x.Value).FirstOrDefault().Key;
+        }
+    }
+}
---
+++ Solutions/MostCommonWord/ReadMe.md
@@ -0,0 +1,4 @@
+﻿<h1 align="center">Most Common Word</h1>
+Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
+
+The words in paragraph are case-insensitive and the answer should be returned in lowercase.
