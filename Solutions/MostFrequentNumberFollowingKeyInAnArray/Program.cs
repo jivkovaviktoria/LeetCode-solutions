b80de558bf0ffd4eb8c4e19307baa54c6dc353ed
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-23 12:28:25 +0200

    Solved " Most Frequent Number Following Key In an Array"

---
+++ Solutions/MostFrequentNumberFollowingKeyInAnArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MostFrequentNumberFollowingKeyInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 100, 200, 1, 100 };
+            int key = 1;
+            Console.WriteLine(MostFrequent(nums, key)); //Expected: 100
+        }
+        
+        public static int MostFrequent(int[] nums, int key) {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+        
+            for(int i = 0; i < nums.Length-1;i++){
+                if(nums[i] == key){
+                    if(d.ContainsKey(nums[i+1]) == false) d.Add(nums[i+1], 0);
+                    d[nums[i+1]]++;
+                }
+            }
+        
+            return d.OrderByDescending(x => x.Value).First().Key;
+        }
+    }
+}
---
+++ Solutions/MostFrequentNumberFollowingKeyInAnArray/ReadMe.md
@@ -0,0 +1,10 @@
+﻿<h1 align="center"> Most Frequent Number Following Key In an Array</h1>
+
+You are given a 0-indexed integer array nums. You are also given an integer key, which is present in nums.
+
+For every unique integer target in nums, count the number of times target immediately follows an occurrence of key in nums. In other words, count the number of indices i such that:
+
+0 <= i <= nums.length - 2,
+nums[i] == key and,
+nums[i + 1] == target.
+Return the target with the maximum count. The test cases will be generated such that the target with maximum count is unique.
