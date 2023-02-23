6aad447323516f06547645c429c46d984940bb22
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-02-23 12:30:52 +0200

    Solved "Single Element in a Sorted Array"

---
+++ Solutions/SingleElementInASortedArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingleElementInASortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 1, 2, 3, 3, 4, 4, 8, 8};
+            Console.WriteLine(SingleNonDuplicate(nums)); //Expected: 2
+        }
+        
+        public static int SingleNonDuplicate(int[] nums) {
+            if(nums.Length == 1) return nums[0];
+        
+            for(int i = 1; i < nums.Length - 1; i++){
+                if(nums[i-1] != nums[i] && nums[i+1] != nums[i]) return nums[i];
+            }
+        
+            if(nums[0] != nums[1]) return nums[0];
+            else return nums[nums.Length-1];
+        }
+    }
+}
---
+++ Solutions/SingleElementInASortedArray/ReadMe.md
@@ -0,0 +1,6 @@
+﻿<h1 align="center">Single Element in a Sorted Array</h1>
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
