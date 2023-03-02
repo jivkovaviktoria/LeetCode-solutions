eb45eb6ec1612796df9d062c0826b8040ee152d0
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-02 20:07:25 +0200

    Solved "Find the Middle Index in Array"

---
+++ Solutions/FindTheMiddleIndexInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FindTheMiddleIndexInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 3, -1, 8, 4 };
+            Console.WriteLine(FindMiddleIndex(nums)); //Expected: 3
+        }
+        
+        public static int FindMiddleIndex(int[] nums) {
+            int sum = 0;
+            foreach(var num in nums) sum += num;
+        
+            int leftSum = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {           
+                if(sum - nums[i] - leftSum == leftSum) return i;
+                leftSum += nums[i];
+            }
+        
+            return -1;
+        }
+    }
+}
---
+++ Solutions/FindTheMiddleIndexInArray/ReadMe.md
@@ -0,0 +1,9 @@
+﻿<h1 align="center">Find the Middle Index in Array</h1>
+
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
