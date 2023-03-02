9e13e38aadd58ac89faf850ea562722f5e0a7f02
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-02 17:58:09 +0200

    Solved "Permutations"

---
+++ Solutions/Permutations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3 };
+            var result = Permute(nums);
+            
+            foreach(var p in result) Console.WriteLine(string.Join(",", p));
+            //Expected: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+        }
+        
+        public static IList<IList<int>> Permute(int[] nums) {
+            List<IList<int>> ans = new List<IList<int>>();
+            Solve(ans, 0, nums);
+            return ans;
+        
+            void Solve(List<IList<int>> ans, int index, int[] nums){
+                if(index == nums.Length){
+                    ans.Add(nums.ToList());
+                    return;
+                }
+            
+                for(int i = index; i < nums.Length; i++){
+                    var temp = nums[index];
+                    nums[index] = nums[i];
+                    nums[i] = temp;
+                
+                    Solve(ans, index + 1, nums);
+                
+                    temp = nums[index];
+                    nums[index] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        
+        }
+    }
+}
---
+++ Solutions/Permutations/ReadMe.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Permutations</h1>
+
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
