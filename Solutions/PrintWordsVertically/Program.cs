5da65b0cc2ef5aecb01b63fc5f9ab20539617404
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-02 18:02:19 +0200

    Solved "Print Words Vertically"

---
+++ Solutions/PrintWordsVertically/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintWordsVertically
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "HOW ARE YOU";
+            Console.WriteLine(string.Join(", ", PrintVertically(s)));
+            //Expected: ["HAY","ORO","WEU"]
+        }
+        
+        public static IList<string> PrintVertically(string s) {
+            var whitespace = ' ';
+            var words = s.Split(whitespace, StringSplitOptions.RemoveEmptyEntries);
+        
+            var max = words.Select(x => x.Length).Max();
+        
+            return Enumerable
+                .Range(0, max)
+                .Select(i => string.Join(string.Empty, words.Select(x => i < x.Length ? x[i] : whitespace)).TrimEnd())
+                .ToList();
+        }
+    }
+}
---
+++ Solutions/PrintWordsVertically/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Print Words Vertically</h1>
+
+Given a string s. Return all the words vertically in the same order in which they appear in s.
+Words are returned as a list of strings, complete with spaces when is necessary. (Trailing spaces are not allowed).
+Each word would be put on only one column and that in one column there will be only one word.
