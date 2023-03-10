4cd73c95d9d5657b6c1d2e9a08b65bca163575be
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-10 19:07:58 +0200

    Solved "DI String Match"

---
+++ Solutions/DIstringMatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DIstringMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "IDID";
+            Console.WriteLine(string.Join(",", DiStringMatch(s)));
+            //Expected: [0,4,1,3,2];
+        }
+        
+        public static int[] DiStringMatch(string s) {
+            int[] ans = new int[s.Length + 1];
+        
+            int l = 0, r = s.Length;
+            for(int i = 0; i < s.Length; i++){
+                if(s[i] == 'I') ans[i] = l++;
+                else ans[i] = r--;
+            }
+        
+            ans[s.Length] = l;
+            return ans;
+        }
+    }
+}
---
+++ Solutions/DIstringMatch/ReadMe.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">DI String Match</h1>
+
+A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
+
+ - `s[i] == 'I' if perm[i] < perm[i + 1]`
+ - `s[i] == 'D' if perm[i] > perm[i + 1]`
+Given a string s, reconstruct the permutation perm and return it. If there are multiple valid permutations perm, return any of them.
