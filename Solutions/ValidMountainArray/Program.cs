122c1b451a84574a4ab939c99f09e596b37d20e2
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-10 19:05:45 +0200

    Solved "Valid Mountain Array"

---
+++ Solutions/ValidMountainArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ValidMountainArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 0, 3, 2, 1 };
+            Console.WriteLine(ValidMountainArray(arr)); //Expected: true
+        }
+        
+        public static bool ValidMountainArray(int[] arr) {
+            if(arr.Length < 3 || (arr[1] > arr[0]) == false) return false;
+        
+            bool isDecreasing = false;
+        
+            for(int i = 1; i < arr.Length; i++){
+                if((arr[i] == arr[i-1]) || (isDecreasing && arr[i] > arr[i-1])) return false;
+                if(arr[i] < arr[i-1]) isDecreasing = true;
+            }
+        
+            return isDecreasing;
+        }
+    }
+}
---
+++ Solutions/ValidMountainArray/ReadMe.md
@@ -0,0 +1,11 @@
+﻿<h1 align="canter">Valid Mountain Array</h1>
+
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+`arr.length >= 3`
+
+There exists some i with 0 < i < arr.length - 1 such that:
+ - `arr[0] < arr[1] < ... < arr[i - 1] < arr[i]`
+ - `arr[i] > arr[i + 1] > ... > arr[arr.length - 1]`
