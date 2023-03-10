92cedbf6b52f51ef19d031a9adf5be651aa74ce5
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-10 19:10:25 +0200

    Solved "Number Of Rectangles That Can Form The Largest Square"

---
+++ Solutions/NumberOfRectanglesThatCanFormTheLargestSquare/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfRectanglesThatCanFormTheLargestSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] rectangles = new int[][]
+            {
+                new int[] { 5, 8 },
+                new int[] { 3, 9 },
+                new int[] { 5, 12 },
+                new int[] { 16, 5 }
+            };
+
+            Console.WriteLine(CountGoodRectangles(rectangles)); //Expected: 3
+        }
+        
+        public static int CountGoodRectangles(int[][] rectangles) {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            int max = -1;
+        
+            for(int i = 0; i < rectangles.Length; i++){
+                var side = Math.Min(rectangles[i][0], rectangles[i][1]);
+                if(d.ContainsKey(side) == false) d.Add(side, 0);
+                d[side]++;
+            
+                if(side > max) max = side;
+            }
+        
+            return d[max];
+        
+        }
+    }
+}
---
+++ Solutions/NumberOfRectanglesThatCanFormTheLargestSquare/ReadMe.md
@@ -0,0 +1,9 @@
+﻿<h1 align="center">Number Of Rectangles That Can Form The Largest Square</h1>
+
+You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
+
+You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example, if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
+
+Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
+
+Return the number of rectangles that can make a square with a side length of maxLen.
