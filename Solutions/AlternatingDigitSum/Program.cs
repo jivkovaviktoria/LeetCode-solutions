867c693cbd7fdd7ea2f9b8bdc8491c579986aad8
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-21 17:40:16 +0200

    Solved "Alternating Digit Sum"

---
+++ Solutions/AlternatingDigitSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlternatingDigitSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 521;
+            Console.WriteLine(AlternateDigitSum(num)); //Expected: 4
+        }
+        
+        public static int AlternateDigitSum(int n) {
+            Stack<int> s = new Stack<int>();
+        
+            while(n > 0){
+                s.Push(n%10);
+                n/=10;
+            }
+        
+            int idx = 0, ans = 0;
+            while(s.Any()){
+                if(idx%2 == 0) ans += s.Pop();
+                else ans -= s.Pop();
+                idx++;
+            }
+        
+            return ans;
+        }
+    }
+}
---
+++ Solutions/AlternatingDigitSum/ReadMe.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">Alternating Digit Sum</h1>
+
+You are given a positive integer n. Each digit of n has a sign according to the following rules:
+
+The most significant digit is assigned a positive sign.
+Each other digit has an opposite sign to its adjacent digits.
+Return the sum of all digits with their corresponding sign.
