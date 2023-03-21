05ecddde439bc8ee6fb6f579bea0ddecf8887585
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-21 17:32:50 +0200

    Solved "Number of Zero-Filled Subarrays"

---
+++ Solutions/NumberOfZero-FilledSubarrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace NumberOfZero_FilledSubarrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 3, 0, 0, 2, 0, 0, 4 };
+            Console.WriteLine(ZeroFilledSubarray(nums)); //Expected: 6
+        }
+        
+        public static long ZeroFilledSubarray(int[] nums) {
+            long ans = 0;
+            int count = 0;
+        
+            for(int i = 0; i < nums.Length; i++){
+                if(nums[i] == 0) count++;
+                else count = 0;
+                ans += count;
+            }
+        
+            return ans;
+        }
+    }
+}
---
+++ Solutions/NumberOfZero-FilledSubarrays/ReadMe.md
@@ -0,0 +1,7 @@
+﻿<h1 align="center">Number of Zero-Filled Subarrays</h1>
+
+Given an integer array `nums`, return the number of subarrays filled with 0.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
