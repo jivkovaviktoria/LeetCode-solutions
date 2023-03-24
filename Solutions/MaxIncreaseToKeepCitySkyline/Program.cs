61c8ac53e1092f70a5cf97dc6edec61330ab4d80
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-24 13:52:46 +0200

    Solved "Max Increase to Keep City Skyline"

---
+++ Solutions/MaxIncreaseToKeepCitySkyline/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MaxIncreaseToKeepCitySkyline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] grid = new int[][] { new int[] { 3, 0, 8, 4 }, new int[] { 2, 4, 5, 7 }, new int[] { 9, 2, 6, 3 }, new int[] { 0, 3, 1, 0 } };
+            Console.WriteLine(MaxIncreaseKeepingSkyline(grid));
+            //Expected: 35
+        }
+        
+        public static int MaxIncreaseKeepingSkyline(int[][] grid) {
+            int max = 0;
+            int[] maxRow = new int[grid.Length], maxCol = new int[grid.Length];
+        
+            for(int i = 0; i < grid.Length; i++){
+                for(int j = 0; j < grid[i].Length; j++){
+                    maxRow[i] = Math.Max(maxRow[i], grid[i][j]);
+                    maxCol[j] = Math.Max(maxCol[j], grid[i][j]);
+                }
+            }
+        
+            for(int i = 0; i < grid.Length; i++){
+                for(int j = 0; j < grid[i].Length; j++)
+                    max += Math.Min(maxRow[i], maxCol[j]) - grid[i][j];
+            }
+        
+            return max;
+        }
+    }
+}
---
+++ Solutions/MaxIncreaseToKeepCitySkyline/ReadMe.md
@@ -0,0 +1,11 @@
+﻿<h1 align="center">Max Increase to Keep City Skyline</h1>
+
+There is a city composed of n x n blocks, where each block contains a single building shaped like a vertical square prism. You are given a 0-indexed n x n integer matrix grid where grid[r][c] represents the height of the building located in the block at row r and column c.
+
+A city's skyline is the the outer contour formed by all the building when viewing the side of the city from a distance. The skyline from each cardinal direction north, east, south, and west may be different.
+
+We are allowed to increase the height of any number of buildings by any amount (the amount can be different per building). The height of a 0-height building can also be increased. However, increasing the height of a building should not affect the city's skyline from any cardinal direction.
+
+Return the maximum total sum that the height of the buildings can be increased by without changing the city's skyline from any cardinal direction.
+
+ 
