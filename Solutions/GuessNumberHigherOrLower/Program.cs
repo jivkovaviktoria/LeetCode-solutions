426d2a25d670fe1e6e849fb727b17d5bfa3c7786
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-24 16:11:58 +0200

    Solved "Guess Number Higher or Lower"

---
+++ Solutions/GuessNumberHigherOrLower/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GuessNumberHigherOrLower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GuessNumber(10)); //Expected: 6
+        }
+        
+        public static int GuessNumber(int n)
+        {
+            int pick = 6; // Picked number
+            
+            int s = 0, e = n;
+        
+            while(s <= e){
+                int mid = s + (e - s)/2;
+            
+                if(guess(mid) == -1) e = mid - 1;
+                else if(guess(mid) == 1) s = mid + 1;
+                else return mid;
+            }
+        
+            return -1;
+
+            int guess(int num)
+            {
+                if (num > pick) return -1;
+                else if (num < pick) return 1;
+                return 0;
+            }
+        }
+    }
+}
---
+++ Solutions/GuessNumberHigherOrLower/ReadMe.md
@@ -0,0 +1,14 @@
+﻿<h1 align="center">Guess Number Higher or Lower</h1>
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+`-1: Your guess is higher than the number I picked (i.e. num > pick).`<br>
+`1: Your guess is lower than the number I picked (i.e. num < pick).`<br>
+`0: your guess is equal to the number I picked (i.e. num == pick).`<br>
+Return the number that I picked.
