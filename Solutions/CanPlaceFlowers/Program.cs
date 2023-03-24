be418ff9253aea982c865f5260b95f9840fbdada
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-03-24 13:55:24 +0200

    Solved "Can Place Flowers"

---
+++ Solutions/CanPlaceFlowers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CanPlaceFlowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] flowerbed = new int[] { 1, 0, 0, 0, 1 };
+            int n = 1;
+            Console.WriteLine(CanPlaceFlowers(flowerbed, n)); //Expected: true
+        }
+        
+        public static bool CanPlaceFlowers(int[] flowerbed, int n) {
+            for(int i = 0; i < flowerbed.Length; i++){
+                if(flowerbed[i] == 0){
+                    int prev = (i == 0) ? 0 : flowerbed[i-1];
+                    int next = (i == flowerbed.Length-1) ? 0 : flowerbed[i+1];
+                
+                    if(prev == 0 && next == 0){
+                        flowerbed[i] = 1;
+                        n--;
+                    }
+                }
+            
+                if(n == 0) return true;
+            }
+        
+            return n <= 0;
+        }
+    }
+}
