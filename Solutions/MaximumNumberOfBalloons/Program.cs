4c6c2f9de036d3de0a72183c8f84cbf315c099e5
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-04-06 12:36:48 +0300

    Solved "Maximum Number of Balloons"

---
+++ Solutions/MaximumNumberOfBalloons/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumNumberOfBalloons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "loonbalxballpoon";
+            Console.WriteLine(MaxNumberOfBalloons(text)); //Expected: 2
+        }
+        
+        public static int MaxNumberOfBalloons(string text) {
+            Dictionary<char, int> d = new Dictionary<char, int>{{'b', 0},{'a', 0},{'l', 0},{'o', 0}, {'n', 0}};
+        
+            foreach(var c in text){
+                if(d.ContainsKey(c)) d[c]++;
+            }
+        
+            d['o']/=2;
+            d['l']/=2;
+        
+            return d.Values.Min();
+        }
+    }
+}
---
+++ Solutions/MaximumNumberOfBalloons/ReadMe.md
@@ -0,0 +1,5 @@
+﻿<h1 align="center">Maximum Number of Balloons</h1>
+
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
