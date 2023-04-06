df52fb40244e5fb2c5ef229e8685766272b1b3eb
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-04-06 12:40:16 +0300

    Solved "Form Smallest Number From Two Digit Arrays"

---
+++ Solutions/FormSmallestNumberFromTwoDigitsArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormSmallestNumberFromTwoDigitsArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 4, 1, 3 }, nums2 = new int[] { 5, 7 };
+            Console.WriteLine(MinNumber(nums1, nums2)); //Expected: 15
+        }
+        
+        public static int MinNumber(int[] nums1, int[] nums2) {
+            HashSet<int> hs = new HashSet<int>(nums1);
+        
+            Array.Sort(nums1);
+            Array.Sort(nums2);
+        
+            for(int i = 0; i < nums2.Length; i++){
+                if(hs.Contains(nums2[i])) return nums2[i];
+            }
+        
+            return Math.Min(nums1[0]*10 + nums2[0], nums2[0]*10 + nums1[0]);
+        }
+    }
+}
---
+++ Solutions/FormSmallestNumberFromTwoDigitsArrays/ReadMe.md
@@ -0,0 +1,3 @@
+﻿<h1 align="center">Form Smallest Number From Two Digit Arrays</h1>
+
+Given two arrays of unique digits nums1 and nums2, return the smallest number that contains at least one digit from each array.
