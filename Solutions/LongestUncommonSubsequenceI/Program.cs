daa1e68d3b9cfa82d47c73ebbba9065383ae3fc1
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-04-06 12:31:55 +0300

    Solved "Longest Uncommon Subsequence I"

---
+++ Solutions/LongestUncommonSubsequenceI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LongestUncommonSubsequenceI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "aba", b = "cdc";
+            Console.WriteLine(FindLUSlength(a, b)); //Expected: 3
+        }
+        
+        //If len(X) > len(Y) then X can't be a subsequence of Y. In this case the longer string is by definition the longest uncommon subsequence.
+        //If len(X) == len(Y) but X != Y then then X can't be a subsequence of Y. In this case both strings are "uncommon subsequences" in respect to each other.
+        //If X == Y then the longest uncommon subsequence doesn't exist.
+        public static int FindLUSlength(string a, string b) {
+            if(a == b) return -1;
+            return Math.Max(a.Length, b.Length);
+        }
+    }
+}
---
+++ Solutions/LongestUncommonSubsequenceI/ReadMe.md
@@ -0,0 +1,9 @@
+﻿<h1>Longest Uncommon Subsequence I</h1>
+
+Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If the longest uncommon subsequence does not exist, return -1.
+
+An uncommon subsequence between two strings is a string that is a subsequence of one but not the other.
+
+A subsequence of a string s is a string that can be obtained after deleting any number of characters from s.
+
+For example, "abc" is a subsequence of "aebdc" because you can delete the underlined characters in "aebdc" to get "abc". Other subsequences of "aebdc" include "aebdc", "aeb", and "" (empty string).
