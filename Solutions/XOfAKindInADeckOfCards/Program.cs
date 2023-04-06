30b1fbe27351d411600fbce9e0b1ee05b83b8bd0
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-04-06 12:34:32 +0300

    Solved "X of a Kind in a Deck of Cards"

---
+++ Solutions/XOfAKindInADeckOfCards/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XOfAKindInADeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] deck = new int[] { 1, 2, 3, 4, 4, 3, 2, 1 };
+            Console.WriteLine(HasGroupsSizeX(deck)); //Expected: true
+        }
+        
+        public static bool HasGroupsSizeX(int[] deck) {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+        
+            foreach(var num in deck){
+                if(d.ContainsKey(num) == false) d.Add(num, 0);
+                d[num]++;
+            }
+        
+            long min = d.Min(x => x.Value);
+            if(min < 2) return false;
+        
+            foreach(var (key, value) in d){
+                min = FindGCD(min, value);
+            }
+        
+            return min > 1;
+        }
+    
+    
+        private static long FindGCD(long a, long b)
+        {
+            if (a == 0) return b;
+            return FindGCD(b % a, a);
+        }
+    }
+}
---
+++ Solutions/XOfAKindInADeckOfCards/ReadMe.md
@@ -0,0 +1,9 @@
+﻿<h1 align="center">X of a Kind in a Deck of Cards</h1>
+
+You are given an integer array deck where deck[i] represents the number written on the ith card.
+
+Partition the cards into one or more groups such that:
+
+Each group has exactly x cards where x > 1, and
+All the cards in one group have the same integer written on them.
+Return true if such partition is possible, or false otherwise.
