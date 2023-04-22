26ffc1b364349eb98c3623c4169bd3f6b251c4a8
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-04-22 10:20:27 +0300

    Solved "Check if Word Equals Summation of Two Words"

---
+++ Solutions/CheckIfWordEqualsSummationOfTwoWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace CheckIfWordEqualsSummationOfTwoWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = "acb", secondWord = "cba", targetWord = "cdb";
+            Console.WriteLine(IsSumEqual(firstWord, secondWord, targetWord)); //Expected: true
+        }
+        
+        public static bool IsSumEqual(string firstWord, string secondWord, string targetWord) {
+            return GetValue(firstWord) + GetValue(secondWord) == GetValue(targetWord);
+        
+            int GetValue(string word){
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i < word.Length; i++){
+                    sb.Append(word[i] - 'a');
+                }
+                return int.Parse(sb.ToString());
+            }
+        }
+    }
+}
