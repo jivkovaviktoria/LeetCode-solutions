48f2f6cd75642910e809f443b5ed7785b78c0f6c
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-04-22 10:24:21 +0300

    Solved "Find the Maximum Divisibility Score"

---
+++ Solutions/FindTheMaximumDivisibilityScore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindTheMaximumDivisibilityScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 4, 7, 9, 3, 9 }, divisors = new int[] { 5, 2, 3 };
+            Console.WriteLine(MaxDivScore(nums, divisors)); //Expected: 3
+        }
+        
+        public static int MaxDivScore(int[] nums, int[] divisors) {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+        
+            foreach(int div in divisors) d[div] = nums.Count(n => n % div == 0);
+        
+            int max = d.Values.Max();
+            int result = d.Where(x => x.Value == max).Min(x => x.Key);
+        
+            return result;
+        }
+    }
+}
---
+++ Solutions/FindTheMaximumDivisibilityScore/ReadMe.md
@@ -0,0 +1,9 @@
+﻿<h1 align="center">Find the Maximum Divisibility Score</h1>
+
+You are given two 0-indexed integer arrays nums and divisors.
+
+The divisibility score of divisors[i] is the number of indices j such that nums[j] is divisible by divisors[i].
+
+Return the integer divisors[i] with the maximum divisibility score. If there is more than one integer with the maximum score, return the minimum of them.
+
+ 
