1d169b1d99cb8b9b7dcbd2afc360c908a63cf459
ViktoriyaV <jivkovaviktoria@gmail.com>
2023-05-21 12:38:18 +0300

    Solved "Minimum String Length After Removing Substrings"

---
+++ Solutions/MinimumStringLengthAfterRemovingSubstrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinimumStringLengthAfterRemovingSubstrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "ABFCACDB";
+            Console.WriteLine(MinLength(s)); //Expected: 2
+        }
+        
+        public static int MinLength(string s) {
+            Stack<char> st = new Stack<char>();
+            for(int i = 0; i < s.Length; i++){
+                if(st.Any() && st.Peek() == 'A' && s[i] == 'B') st.Pop();
+                else if(st.Any() && st.Peek() == 'C' && s[i] == 'D') st.Pop();
+                else st.Push(s[i]);
+            }
+        
+            return st.Count;
+        }
+    }
+}
---
+++ Solutions/MinimumStringLengthAfterRemovingSubstrings/ReadMe.md
@@ -0,0 +1,9 @@
+﻿<h1 align="center">Minimum String Length After Removing Substrings</h1>
+
+You are given a string `s` consisting only of uppercase English letters.
+
+You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings `"AB"` or `"CD"` from `s`.
+
+Return the minimum possible length of the resulting string that you can obtain.
+
+Note that the string concatenates after removing the substring and could produce new `"AB"` or `"CD"` substrings.
