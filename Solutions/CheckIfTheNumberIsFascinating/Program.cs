1a5f1e95d463d52964e328fc3e4f978f1cc5300e
ViktoriyaV <jivkovaviktoria@gmail.com>
2023-06-11 09:55:13 +0300

    Solved "Check if The Number is Fascinating"

---
+++ Solutions/CheckIfTheNumberIsFascinating/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckIfTheNumberIsFascinating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 192;
+            Console.WriteLine(IsFascinating(num)); //Expected: true
+        }
+        
+        public static bool IsFascinating(int x1) {
+            HashSet<int> nums = new HashSet<int>(){1, 2, 3, 4, 5, 6, 7, 8, 9};
+        
+            var x2 = x1*2;
+            var x3 = x1*3;
+        
+            while(x1 > 0 && x2 > 0 && x3 > 0){
+                nums.Remove(x1%10);
+                nums.Remove(x2%10);
+                nums.Remove(x3%10);
+            
+                x1/=10;
+                x2/=10;
+                x3/=10;
+            }
+        
+            return nums.Count == 0;
+        }
+    }
+}
---
+++ Solutions/CheckIfTheNumberIsFascinating/ReadMe.md
@@ -0,0 +1,10 @@
+﻿<h1 align="center">Check If Number Is Fascinating</h1>
+
+You are given an integer `n` that consists of exactly 3 digits.
+
+We call the number `n` fascinating if, after the following modification, the resulting number contains all the digits from 1 to 9 exactly once and does not contain any 0's:
+
+Concatenate `n` with the numbers `2 * n` and `3 * n`.
+Return `true` if `n` is fascinating, or `false` otherwise.
+
+Concatenating two numbers means joining them together. For example, the concatenation of `121` and `371` is `121371`.
