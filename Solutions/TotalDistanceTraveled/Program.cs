d98ad68be42b7da4f35da4cb75861027c30651df
ViktoriyaV <jivkovaviktoria@gmail.com>
2023-06-19 08:35:53 +0300

    Solved "Total Distance Traveled"

---
+++ Solutions/TotalDistanceTraveled/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TotalDistanceTraveled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mainTank = 5, additionalTank = 10;
+            Console.WriteLine(DistanceTraveled(mainTank, additionalTank));
+            //Expected: 60
+        }
+        
+        public static int DistanceTraveled(int mainTank, int additionalTank) {
+            int ans = 0;
+            while(mainTank >= 5 && additionalTank > 0){
+                ans += 50;
+                mainTank -= 5;
+                mainTank++;
+                additionalTank--;
+            }
+        
+            while(mainTank-- > 0)
+                ans+=10;
+        
+            return ans;
+        }
+    }
+}
---
+++ Solutions/TotalDistanceTraveled/ReadMe.md
@@ -0,0 +1,9 @@
+﻿<h1 align="center">Total Distance Traveled</h1>
+
+A truck has two fuel tanks. You are given two integers, mainTank representing the fuel present in the main tank in liters and additionalTank representing the fuel present in the additional tank in liters.
+
+The truck has a mileage of 10 km per liter. Whenever 5 liters of fuel get used up in the main tank, if the additional tank has at least 1 liters of fuel, 1 liters of fuel will be transferred from the additional tank to the main tank.
+
+Return the maximum distance which can be traveled.
+
+Note: Injection from the additional tank is not continuous. It happens suddenly and immediately for every 5 liters consumed.
