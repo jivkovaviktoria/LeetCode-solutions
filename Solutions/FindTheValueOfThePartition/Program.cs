c26b864cb5a94f897eb1b3037d9efa45ecee2721
ViktoriyaV <jivkovaviktoria@gmail.com>
2023-06-19 08:42:49 +0300

    Solved "Find the Value of the Partition"

---
+++ Solutions/FindTheValueOfThePartition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FindTheValueOfThePartition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 3, 2, 4};
+            Console.WriteLine(FindValueOfPartition(nums)); //Expected: 1
+        }
+        
+        public static int FindValueOfPartition(int[] nums) {
+            Array.Sort(nums);
+        
+            var min = int.MaxValue;
+            for(int i = 1; i < nums.Length; i++){
+                min = Math.Min(Math.Abs(nums[i] - nums[i-1]), min);
+            }
+        
+            return min;
+        }
+    }
+}
---
+++ Solutions/FindTheValueOfThePartition/ReadMe.md
@@ -0,0 +1,14 @@
+﻿<h1 align="center">Find the Value of the Partition</h1>
+
+You are given a positive integer array nums.
+
+Partition nums into two arrays, nums1 and nums2, such that:
+
+Each element of the array nums belongs to either the array nums1 or the array nums2.
+Both arrays are non-empty.
+The value of the partition is minimized.
+The value of the partition is |max(nums1) - min(nums2)|.
+
+Here, max(nums1) denotes the maximum element of the array nums1, and min(nums2) denotes the minimum element of the array nums2.
+
+Return the integer denoting the value of such partition.
