7b06f5b0870a4b3891a86d63effcd2bcaca5786c
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-12-25 22:26:58 +0200

    Solved 'Minimum Number Game'

---
+++ Solutions/MinimumNumberGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MinimumNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 5, 4, 2, 3 };
+            Console.WriteLine(string.Join(", ", NumberGame(nums))); //Expected: [3, 2, 5, 4]
+        }
+        
+        public static int[] NumberGame(int[] nums) {
+            int[] res = new int[nums.Length];
+        
+            Array.Sort(nums);
+            for(int i = 0; i < nums.Length-1; i+=2){
+                res[i] = nums[i+1];
+                res[i+1] = nums[i];
+            }
+        
+            return res;
+        }
+    }
+}
