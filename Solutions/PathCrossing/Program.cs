68b4bb1ec963637efb848fa8a33d109b99879905
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-12-25 22:24:17 +0200

    Solved 'Path Crossing'

---
+++ Solutions/PathCrossing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AddDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "NESWW";
+            Console.WriteLine(IsPathCrossing(path)); //Expected: True
+        }
+        
+        public static bool IsPathCrossing(string path) {
+            HashSet<(int x, int y)> moves = new(){(0, 0)};
+        
+            int x = 0, y = 0;
+        
+            for(int i = 0; i < path.Length; i++){
+                if(path[i] == 'N') y++;
+                else if(path[i] == 'S') y--;
+                else if(path[i] == 'E') x++;
+                else x--;
+            
+                if(moves.Contains((x, y))) return true;
+            
+                moves.Add((x, y));
+            }
+        
+            return false;
+        }
+    }
+}
